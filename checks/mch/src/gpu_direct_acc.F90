1baa4e2bec5252cc950c64b50975cd51783f8342
Xavier Lapillonne <xavier.lapillonne@meteoswiss.ch>
2022-08-29 14:58:37 +0200

    GPU only test, use mpi - test will fail if not compiled with OpenACC support - replace depreciated include "mpif.h" with use mpi

--- checks/mch/src/gpu_direct_acc.F90
+++ checks/mch/src/gpu_direct_acc.F90
@@ -1,9 +1,10 @@
 program GpuDirectAcc
     ! This code tests MPI communication on GPU with OpenACC using the
-    ! host_data directive
-    implicit none
+    ! host_data directive - the test will fail of not compiled with OpenACC
 
-    include 'mpif.h'
+    use mpi
+
+    implicit none
 
     integer :: ierr, status(MPI_STATUS_SIZE), i
     integer :: mpi_size, mpi_rank
@@ -15,31 +16,42 @@ program GpuDirectAcc
     call MPI_Comm_rank(MPI_COMM_WORLD, mpi_rank, ierr)
 
     mydata(1) = mpi_rank
+    data_sum(1)=0
+
 #ifdef _OPENACC
     if (mpi_rank==0) write(*,*) "MPI test on GPU with OpenACC using ",mpi_size,"tasks"
 #else
-    if (mpi_rank==0) write(*,*) "MPI test on CPU using ",mpi_size,"tasks"
+    if (mpi_rank==0) write (*,*) "Error : test not compiled with OpenACC"
+    call mpi_abort(MPI_COMM_WORLD, -1, ierr) 
 #endif
 
-    !$acc data copy(mydata,data_sum)
+    !$acc data create(mydata,data_sum)
+    !$acc update device(mydata,data_sum)
+    ! Errase mydata on CPU to make sure only GPU values are used
+    mydata(1)=-9999
+
     !$acc host_data use_device(mydata,data_sum)
     call MPI_Reduce(mydata, data_sum, 1, MPI_INTEGER8, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
     !$acc end host_data
-    !$acc end data
+
+    !$acc update host(data_sum)
+
 
     !Check results
     if (mpi_rank.eq.0) then
       ref_val=0
+      !$acc parallel loop reduction(+:ref_val)
       do i=0,mpi_size-1
         ref_val=ref_val+i
       end do
       if (data_sum(1)/=ref_val) then
         write (*,*) "Result : FAIL"
-        write (*,*) "Expected value : ", ref_val, "actual value:",data_sum(1)
+        write (*,*) "Expected vwalue : ", ref_val, "actual value:",data_sum(1)
       else
         write (*,*) "Result : OK"
       end if
     end if
+    !$acc end data
 
     call MPI_Finalize(ierr);
 
