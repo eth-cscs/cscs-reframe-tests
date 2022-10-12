e0639ea551c1586d0ae4edd50aa3bc94b7983919
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2022-10-12 14:33:13 +0200

    Fix typo

--- checks/microbenchmarks/gpu/shmem/shmem.py
+++ checks/microbenchmarks/gpu/shmem/shmem.py
@@ -45,7 +45,7 @@ class gpu_shmem_check(GpuShmem):
             'bandwidth': (9060, -0.01, None, 'GB/s')
         }
     }
-    maintainers = ['@sebkell']
+    maintainers = ['@sekelle']
     tags = {'benchmark', 'diagnostic', 'craype', 'health'}
 
     # Inject external hooks
