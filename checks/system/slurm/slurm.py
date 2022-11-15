02b1f2edd5e51ec522fd7fad6b352cf8cc5a1156
jgp <jgp@cscs.ch>
2022-11-15 12:50:06 +0100

    fix for review

--- checks/system/slurm/slurm.py
+++ checks/system/slurm/slurm.py
@@ -196,7 +196,7 @@ class MemoryOverconsumptionCheck(SlurmCompiledBaseCheck):
     sourcepath = 'eatmemory.c'
     executable_opts = ['4000M']
 
-    @run_before('compile')
+    @run_after('init')
     def set_skip(self):
         self.skip_if(self.current_partition.name == 'login',
                      'MemoryOverconsumptionCheck not needed on login node')
