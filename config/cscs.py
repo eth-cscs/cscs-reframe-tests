970bbb10b8e31cc3f2d8d88989a424a26908c734
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2023-02-23 10:40:18 +0100

    Move files to systems directory

--- config/cscs.py
+++ config/cscs.py
@@ -1,20 +1,23 @@
-# Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# Copyright 2016-2023 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
 # ReFrame Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # ReFrame CSCS settings
 #
+import glob
 import os
 from reframe.utility import import_module_from_file
 
-systems = ['common', 'ault', 'daint', 'eiger', 'hohgant']
-# TDS systems are already included in the production systems,
-# for example dom is included in the daint configuration file
+
+system_conf_files = glob.glob(
+    os.path.join(os.path.dirname(__file__), 'systems', '*.py')
+)
 config_files = [
-    os.path.join(os.path.dirname(__file__), f"{sys}.py") for sys in systems
+    os.path.join(os.path.dirname(__file__), 'common.py')
 ]
-# print(config_files)
+# Filter out the links
+config_files += [s for s in system_conf_files if not os.path.islink(s)]
 system_configs = [
     import_module_from_file(f).site_configuration for f in config_files
 ]
