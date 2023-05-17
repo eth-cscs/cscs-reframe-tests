6412428777623c53b8f9f89833de6702e196bf91
Theofilos Manitaras <manitaras@cscs.ch>
2023-05-17 10:32:08 +0200

    Update Sarus PyFR test
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

--- checks/containers/sarus/check_pyfr.py
+++ checks/containers/sarus/check_pyfr.py
@@ -22,22 +22,35 @@ class SarusPyFRCheck(rfm.RunOnlyRegressionTest):
             self.valid_systems = ['+sarus']
 
     @run_after('setup')
+    def set_cuda_visible_devices(self):
+        if self.backend == 'cuda':
+            curr_part = self.current_partition
+            self.gpu_count = curr_part.select_devices('gpu')[0].num_devices
+            cuda_visible_devices = ','.join(f'{i}' for i in range(self.gpu_count))
+            self.env_vars['CUDA_VISIBLE_DEVICES'] = cuda_visible_devices
+
+    @run_after('setup')
     def setup_container_platform(self):
-        self.container_platform.image = ('ethcscs/pyfr:'
-                                         '1.5.0-cuda8.0-ubuntu16.04')
-        self.prerun_cmds = ['sarus --version']
-        import_mesh_command = ('pyfr import ./euler_vortex_2d.msh '
-                               './euler_vortex_2d.pyfrm')
-        modify_ini_command = (
-            r"sed -e 's/^;cblas.*/cblas = \/usr\/lib\/libopenblas.so/' "
-            r"-i ./euler_vortex_2d.ini"
-        )
+        image_tag = '1.15.0-cpu-mpich4.1-ubuntu22.04'
+        if self.backend == 'cuda':
+            image_tag = '1.14.0-cuda11.7-mpich3.1.4-ubuntu22.04'
+
+        self.container_platform.image = f'ethcscs/pyfr:{image_tag}'
+
+        self.prerun_cmds = [
+             r'sarus --version',
+             r'wget -q https://raw.githubusercontent.com/PyFR/PyFR-Test-Cases/'
+             r'main/2d-euler-vortex/euler-vortex.ini',
+             r'wget -q https://raw.githubusercontent.com/PyFR/PyFR-Test-Cases/'
+             r'main/2d-euler-vortex/euler-vortex.msh',
+        ]
+        import_mesh_command = ('pyfr import ./euler-vortex.msh '
+                               './euler-vortex.pyfrm')
         compute_command = (f'pyfr run --backend {self.backend} '
-                           f'./euler_vortex_2d.pyfrm ./euler_vortex_2d.ini')
+                           f'./euler-vortex.pyfrm ./euler-vortex.ini')
         self.container_platform.command = (
-            f'bash -c "cd /PyFR-1.5.0/examples/euler_vortex_2d && '
-            f'{import_mesh_command} && {modify_ini_command} && '
-            f'{compute_command} && echo CHECK SUCCESSFUL"'
+            f'bash -c "{import_mesh_command} && {compute_command} && '
+            f'echo CHECK SUCCESSFUL"'
         )
 
     @sanity_function
