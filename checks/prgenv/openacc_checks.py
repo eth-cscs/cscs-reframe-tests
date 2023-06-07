a3c1f0b3bf52b2ed61b117195db26892984286d2
jg <jgp@cscs.ch>
2023-06-07 15:34:31 +0200

    update

--- checks/prgenv/openacc_checks.py
+++ checks/prgenv/openacc_checks.py
@@ -9,11 +9,11 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class OpenACCFortranCheck(rfm.RegressionTest):
-    variant = parameter(['mpi', 'nompi'])
+    variant = parameter(['mpi'])
     valid_systems = ['+nvgpu']
     sourcesdir = 'src/openacc'
     build_system = 'SingleSource'
-    modules = ['cudatoolkit']
+    num_tasks = 1
     num_gpus_per_node = 1
     num_tasks_per_node = 1
     tags = {'production', 'craype'}
@@ -25,31 +25,33 @@ class OpenACCFortranCheck(rfm.RegressionTest):
             self.valid_prog_environs = ['+openacc +mpi']
 
     @run_after('init')
-    def set_numtasks(self):
-        if self.variant == 'nompi':
-            self.num_tasks = 1
-            self.sourcepath = 'vecAdd_openacc_nompi.f90'
-        else:
+    def set_mpi(self):
+        self.sourcepath = 'vecAdd_openacc.F90'
+        if self.variant == 'mpi':
+            self.build_system.fflags = ['-D_MPI']
             self.num_tasks = 2
-            self.sourcepath = 'vecAdd_openacc_mpi.f90'
 
     @run_after('setup')
-    def set_modules(self):
+    def set_compilers(self):
         curr_part = self.current_partition
         gpu_arch = curr_part.select_devices('gpu')[0].arch
-
         # Remove the '^sm_' prefix from the arch, e.g sm_80 -> 80
         if gpu_arch.startswith('sm_'):
             accel_compute_capability = gpu_arch[len('sm_'):]
         else:
             accel_compute_capability = '80'
 
-        self.modules += [f'craype-accel-nvidia{accel_compute_capability}']
-
-    @run_before('run')
-    def set_mpi_gpu_support(self):
-        if self.variant == 'mpi':
-            self.env_vars['MPICH_GPU_SUPPORT_ENABLED'] = 1
+        self.modules += (
+            ["cudatoolkit", f"craype-accel-nvidia{accel_compute_capability}"]
+            if self.current_environ.name == "PrgEnv-cray"
+            else ""
+        )
+        acc_flags = {
+            'PrgEnv-nvidia': [
+                '-acc', f'-ta=tesla:cc{accel_compute_capability}'],
+            'PrgEnv-cray': ['-hacc', '-hnoomp'],
+        }
+        self.build_system.ldflags = acc_flags[self.current_environ.name]
 
     @run_before('run')
     def set_launcher_options(self):
@@ -64,8 +66,20 @@ class OpenACCFortranCheck(rfm.RegressionTest):
         cuda_visible_devices = ','.join(f'{i}' for i in range(self.gpu_count))
         self.env_vars['CUDA_VISIBLE_DEVICES'] = cuda_visible_devices
 
-    @sanity_function
-    def assert_correct_result(self):
-        result = sn.extractsingle(r'final result:\s+(?P<result>\d+\.?\d*)',
-                                  self.stdout, 'result', float)
-        return sn.assert_reference(result, 1., -1e-5, 1e-5)
+    @run_before('run')
+    def set_debug(self):
+        self.env_vars['NVCOMPILER_ACC_NOTIFY'] = '1'
+        self.env_vars['CRAY_ACC_DEBUG'] = '1'
+
+    @run_before('sanity')
+    def set_sanity(self):
+        regex_result = r'final result:\s+1'
+        regex_version = r'_OPENACC:\s+(\d+)'
+        regex_nv = r'num_gangs=\d+ num_workers=\d+ vector_length=\d+ grid='
+        regex_cce = r'to acc 0 bytes, to host \d+ bytes'
+        regex_kernel = f'({regex_nv}|{regex_cce})'
+        self.sanity_patterns = sn.all([
+            sn.assert_found(regex_result, self.stdout),
+            sn.assert_found(regex_version, self.stdout),
+            sn.assert_found(regex_kernel, self.stderr),
+        ])
