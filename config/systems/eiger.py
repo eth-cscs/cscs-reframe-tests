e64ca274098a2d93c9bd9aff4278dba8e526f98a
Luca <lucamar@cscs.ch>
2023-06-13 16:43:56 +0200

    Update Eiger configuration after 14.06 maintenance

--- config/systems/eiger.py
+++ config/systems/eiger.py
@@ -44,7 +44,7 @@ base_config = {
                 },
                 {
                     'type': 'Singularity',
-                    'modules': ['singularity/3.5.3-eiger']
+                    'modules': ['cray', 'singularity/3.5.3-eiger']
                 }
             ],
             'environs': [
@@ -79,72 +79,55 @@ base_config = {
     ]
 }
 
-eiger_sys = copy.deepcopy(base_config)
-eiger_sys['name'] = 'eiger'
-eiger_sys['descr'] = 'Alps Cray EX Supercomputer'
-eiger_sys['hostnames'] = ['eiger']
-eiger_sys['partitions'].append(
-    {
-        'name': 'jupyter_mc',
-        'scheduler': 'slurm',
-        'environs': ['builtin'],
-        'access': [
-            f'-Cmc',
-            f'--reservation=interact',
-            f'--account={osext.osgroup()}'
-        ],
-        'descr': 'JupyterHub multicore nodes',
-        'max_jobs': 10,
-        'launcher': 'srun',
-        'features': ['remote'],
-    }
-)
-
+pilatus_sys = copy.deepcopy(base_config)
+pilatus_sys['name'] = 'pilatus'
+pilatus_sys['descr'] = 'Alps Cray EX Supercomputer TDS'
+pilatus_sys['hostnames'] = ['pilatus']
 
 site_configuration = {
     'systems': [
-        eiger_sys,
+        pilatus_sys
     ],
     'environments': [
         {
             'name': 'PrgEnv-aocc',
-            'target_systems': ['eiger'],
-            'modules': ['PrgEnv-aocc']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'PrgEnv-aocc']
         },
         {
             'name': 'PrgEnv-cray',
-            'target_systems': ['eiger'],
-            'modules': ['PrgEnv-cray']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'PrgEnv-cray']
         },
         {
             'name': 'PrgEnv-gnu',
-            'target_systems': ['eiger'],
-            'modules': ['PrgEnv-gnu']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'PrgEnv-gnu']
         },
         {
             'name': 'PrgEnv-intel',
-            'target_systems': ['eiger'],
-            'modules': ['PrgEnv-intel']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'PrgEnv-intel']
         },
         {
             'name': 'cpeAMD',
-            'target_systems': ['eiger'],
-            'modules': ['cpeAMD']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'cpeAMD']
         },
         {
             'name': 'cpeCray',
-            'target_systems': ['eiger'],
-            'modules': ['cpeCray']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'cpeCray']
         },
         {
             'name': 'cpeGNU',
-            'target_systems': ['eiger'],
-            'modules': ['cpeGNU']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'cpeGNU']
         },
         {
             'name': 'cpeIntel',
-            'target_systems': ['eiger'],
-            'modules': ['cpeIntel']
+            'target_systems': ['pilatus'],
+            'modules': ['cray', 'cpeIntel']
         },
     ],
 }
