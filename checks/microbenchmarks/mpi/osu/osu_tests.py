3896e68b84840bcd6701e6e72653773065685e01
Theofilos Manitaras <manitaras@cscs.ch>
2023-07-07 16:57:15 +0200

    Revisit the osu microbenchmarks for cpe
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

--- checks/microbenchmarks/mpi/osu/osu_tests.py
+++ checks/microbenchmarks/mpi/osu/osu_tests.py
@@ -1,11 +1,20 @@
-# Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# Copyright 2016-2023 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
 # ReFrame Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
 import contextlib
-import reframe as rfm
+import pathlib
+import sys
 
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+sys.path.append(
+    str(pathlib.Path(__file__).parent.parent.parent.parent / 'mixins')
+)
+
+from extra_launcher_options import ExtraLauncherOptionsMixin
 from hpctestlib.microbenchmarks.mpi.osu import (build_osu_benchmarks,
                                                 osu_build_run)
 
@@ -13,94 +22,88 @@ from hpctestlib.microbenchmarks.mpi.osu import (build_osu_benchmarks,
 class cscs_build_osu_benchmarks(build_osu_benchmarks):
     build_type = parameter(['cpu', 'cuda'])
 
+    @run_after('setup')
+    def setup_compilers(self):
+        if self.build_type == 'cuda':
+            curr_part = self.current_partition
+            gpu_arch = curr_part.select_devices('gpu')[0].arch
+            self.build_system.ldflags = [
+                '${CRAY_CUDATOOLKIT_POST_LINK_OPTS}',
+                '-L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_cuda'
+            ]
+
+            # Remove the '^sm_' prefix from the arch, e.g sm_80 -> 80
+            if gpu_arch.startswith('sm_'):
+                accel_compute_capability = gpu_arch[len('sm_'):]
+            else:
+                accel_compute_capability = '80'
+
+            if self.current_environ.name in {'PrgEnv-cray', 'PrgEnv-gnu'}:
+                self.modules = [
+                    'cudatoolkit',
+                    f'craype-accel-nvidia{accel_compute_capability}'
+                ]
+
+
+class cscs_osu_benchmarks(osu_build_run, ExtraLauncherOptionsMixin):
+    osu_binaries = fixture(cscs_build_osu_benchmarks, scope='environment')
+    num_iters = 100
+
     @run_after('init')
-    def setup_modules(self):
-        if self.build_type != 'cuda':
-            return
+    def set_valid_systems_envs(self):
+        build_type = self.osu_binaries.build_type
+        if build_type == 'cuda':
+            self.valid_systems = ['+remote +nvgpu']
+            self.valid_prog_environs = ['+mpi +cuda']
+        else:
+            self.valid_systems = ['+remote']
+            self.valid_prog_environs = ['+mpi']
 
-        if self.current_system.name in ('daint', 'dom'):
-            self.modules = ['cudatoolkit/21.3_11.2']
-        elif self.current_system.name in ('arolla', 'tsa'):
-            self.modules = ['cuda/10.1.243']
-            self.build_system.ldflags = ['-L$EBROOTCUDA/lib64',
-                                         '-lcudart', '-lcuda']
+    @run_before('run')
+    def set_environment(self):
+        build_type = self.osu_binaries.build_type
+        if build_type == 'cuda':
+            self.env_vars = {
+                # Enable GPU support for cray-mpich
+                'MPICH_GPU_SUPPORT_ENABLED': 1,
 
+                # Use only the first CUDA GPU
+                'CUDA_VISIBLE_DEVICES': 0,
+            }
 
-class cscs_osu_benchmarks(osu_build_run):
     exclusive_access = True
-    tags = {'production', 'benchmark', 'craype'}
-    maintainers = ['@rsarm']
+    tags = {'cpe'}
 
 
 @rfm.simple_test
 class cscs_osu_pt2pt_check(cscs_osu_benchmarks):
-    valid_systems = ['daint:gpu', 'daint:mc', 'dom:gpu', 'dom:mc',
-                     'eiger:mc', 'pilatus:mc', 'arolla:cn', 'tsa:cn']
-    valid_prog_environs = ['PrgEnv-gnu']
     benchmark_info = parameter([
         ('mpi.pt2pt.osu_bw', 'bandwidth'),
         ('mpi.pt2pt.osu_latency', 'latency')
     ], fmt=lambda x: x[0], loggable=True)
-    osu_binaries = fixture(cscs_build_osu_benchmarks, scope='environment')
     allref = {
         'mpi.pt2pt.osu_bw': {
             'cpu': {
-                'daint:gpu': {
-                    'bandwidth': (9153.637, -0.10, None, 'MB/s')
-                },
-                'daint:mc': {
-                    'bandwidth': (7650.943, -0.10, None, 'MB/s')
-                },
-                'dom:gpu': {
-                    'bandwidth': (9140.621, -0.10, None, 'MB/s')
-                },
-                'dom:mc': {
-                    'bandwidth': (8632.197, -0.10, None, 'MB/s')
-                },
-                'eiger:mc': {
-                    'bandwidth': (12240.41, -0.10, None, 'MB/s')
-                },
-                'pilatus:mc': {
-                    'bandwidth': (12240.476, -0.10, None, 'MB/s')
+                '*': {
+                    'bandwidth': (24000.0, -0.10, None, 'MB/s')
                 }
             },
             'cuda': {
-                'daint:gpu': {
-                    'bandwidth': (8431.02, -0.10, None, 'MB/s')
-                },
-                'dom:gpu': {
-                    'bandwidth': (8637.693, -0.10, None, 'MB/s')
+                '*': {
+                    'bandwidth': (24000.0, -0.10, None, 'MB/s')
                 }
             }
         },
         'mpi.pt2pt.osu_latency': {
             'cpu': {
-                'daint:gpu': {
-                    'latency': (1.651, None, 0.10, 'us')
-                },
-                'daint:mc': {
-                    'latency': (2.175, None, 0.10, 'us')
-                },
-                'dom:gpu': {
-                    'latency': (1.314, None, 0.10, 'us')
-                },
-                'dom:mc': {
-                    'latency': (1.439, None, 0.10, 'us')
-                },
-                'eiger:mc': {
-                    'latency': (2.76, None, 0.15, 'us')
-                },
-                'pilatus:mc': {
-                    'latency': (2.413, None, 0.15, 'us')
+                '*': {
+                    'latency': (2.0, None, 0.15, 'us')
                 }
             },
             'cuda': {
-                'daint:gpu': {
-                    'latency': (8.907, None, 0.10, 'us')
-                },
-                'dom:gpu': {
-                    'latency': (6.043, None, 0.10, 'us')
-                },
+                '*': {
+                    'latency': (10.0, None, 0.15, 'us')
+                }
             }
         }
     }
@@ -111,14 +114,7 @@ class cscs_osu_pt2pt_check(cscs_osu_benchmarks):
         if build_type == 'cuda':
             self.device_buffers = 'cuda'
             self.num_gpus_per_node = 1
-            self.valid_systems = ['daint:gpu',
-                                  'dom:gpu', 'arolla:cn', 'tsa:cn']
-            if self.current_system.name in ('daint', 'dom'):
-                self.valid_prog_environs = ['PrgEnv-nvidia']
-                self.env_vars = {'MPICH_RDMA_ENABLED_CUDA': 1}
-
-        if self.current_system.name in {'arolla', 'tsa'}:
-            self.num_iters = 100
+            self.env_vars = {'MPICH_RDMA_ENABLED_CUDA': 1}
 
         with contextlib.suppress(KeyError):
             self.reference = self.allref[self.benchmark_info[0]][build_type]
@@ -130,76 +126,29 @@ class cscs_osu_collective_check(cscs_osu_benchmarks):
         ('mpi.collective.osu_alltoall', 'latency'),
         ('mpi.collective.osu_allreduce', 'latency'),
     ], fmt=lambda x: x[0], loggable=True)
-    num_nodes = parameter([6, 16])
-    extra_resources = {
-        'switches': {
-            'num_switches': 1
-        }
-    }
-    valid_systems = ['daint:gpu', 'daint:mc']
-    valid_prog_environs = ['PrgEnv-gnu']
+
+    num_nodes = parameter([6])
     osu_binaries = fixture(cscs_build_osu_benchmarks, scope='environment')
+
     allref = {
         'mpi.collective.osu_allreduce': {
             6: {
-                'dom:gpu': {
-                    'latency': (6.26, None, 0.10, 'us')
+                '*': {
+                    'latency': (8.45, None, 0.10, 'us')
                 },
-                'dom:mc': {
-                    'latency': (7.235, None, 0.10, 'us')
-                },
-                'daint:gpu': {
-                    'latency': (18.012, None, 0.10, 'us')
-                },
-                'daint:mc': {
-                    'latency': (18.016, None, 0.10, 'us')
-                }
             },
-            16: {
-                'daint:gpu': {
-                    'latency': (28.925, None, 0.10, 'us')
-                },
-                'daint:mc': {
-                    'latency': (39.147, None, 0.10, 'us')
-                }
-            }
         },
         'mpi.collective.osu_alltoall': {
             6: {
-                'dom:gpu': {
-                    'latency': (8.23, None, 0.10, 'us')
+                '*': {
+                    'latency': (14.50, None, 0.10, 'us')
                 },
-                'daint:gpu': {
-                    'latency': (20.73, None, 0.10, 'us')
-                },
-                'dom:mc': {
-                    'latency': (0, None, None, 'us')
-                },
-                'daint:mc': {
-                    'latency': (0, None, None, 'us')
-                }
             },
-            16: {
-                'daint:gpu': {
-                    'latency': (0, None, None, 'us')
-                },
-                'daint:mc': {
-                    'latency': (0, None, None, 'us')
-                }
-            }
         }
     }
 
     @run_after('init')
     def setup_by_scale(self):
-        if self.osu_binaries.build_type == 'cuda':
-            # Filter out CUDA-aware versions
-            self.valid_systems = []
-            return
-
         self.num_tasks = self.num_nodes
-        if self.num_nodes == 6:
-            self.valid_systems += ['dom:gpu', 'dom:mc']
-
         with contextlib.suppress(KeyError):
             self.reference = self.allref[self.num_nodes]
