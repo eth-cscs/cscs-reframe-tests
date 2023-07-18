f9c4a5771ec2018fc0b86bb65a38173310e9d17f
Theofilos Manitaras <manitaras@cscs.ch>
2023-07-18 16:41:33 +0200

    Refactor to break dependency with hpctestlib
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

--- checks/microbenchmarks/mpi/osu/osu_tests.py
+++ checks/microbenchmarks/mpi/osu/osu_tests.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import contextlib
+import os
 import pathlib
 import sys
 
@@ -15,22 +16,59 @@ sys.path.append(
 )
 
 from extra_launcher_options import ExtraLauncherOptionsMixin
-from hpctestlib.microbenchmarks.mpi.osu import (build_osu_benchmarks,
-                                                osu_build_run)
 
 
-class cscs_build_osu_benchmarks(build_osu_benchmarks):
+class fetch_osu_benchmarks(rfm.RunOnlyRegressionTest):
+    '''Fixture for fetching the OSU benchmarks.'''
+
+    #: The version of the benchmarks to fetch.
+    #:
+    #: :type: :class:`str`
+    #: :default: ``'7.2'``
+    version = variable(str, value='7.2')
+
+    local = True
+    osu_file_name = f'osu-micro-benchmarks-{version}.tar.gz'
+    executable = f'curl -LJO http://mvapich.cse.ohio-state.edu/download/mvapich/{osu_file_name}'  # noqa: E501
+
+    @sanity_function
+    def validate_download(self):
+        return sn.assert_eq(self.job.exitcode, 0)
+
+
+class build_osu_benchmarks(rfm.CompileOnlyRegressionTest):
+    '''Fixture for building the OSU benchmarks'''
+
+    #: Build variant parameter.
+    #:
+    #: :type: :class:`str`
+    #: :values: ``'cpu', 'cuda'``
     build_type = parameter(['cpu', 'cuda'])
+
+    build_system = 'Autotools'
+    build_prefix = variable(str)
+
+    #: The fixture object that retrieves the benchmarks
+    #:
+    #: :type: :class:`fetch_osu_benchmarks`
+    #: :scope: *session*
+    osu_benchmarks = fixture(fetch_osu_benchmarks, scope='session')
 
     @run_after('setup')
     def setup_compilers(self):
         if self.build_type == 'cuda':
             curr_part = self.current_partition
             gpu_arch = curr_part.select_devices('gpu')[0].arch
-            self.build_system.ldflags = [
-                '${CRAY_CUDATOOLKIT_POST_LINK_OPTS}',
-                '-L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_cuda'
-            ]
+            if self.current_environ.name != 'PrgEnv-nvhpc':
+                self.build_system.ldflags = [
+                    '${CRAY_CUDATOOLKIT_POST_LINK_OPTS}',
+                    '-L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_cuda'
+                ]
+            else:
+                self.build_system.ldflags = [
+                    '-L${CRAY_NVIDIA_PREFIX}/cuda/lib64',
+                    '-L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_cuda'
+                ]
 
             # Remove the '^sm_' prefix from the arch, e.g sm_80 -> 80
             if gpu_arch.startswith('sm_'):
@@ -45,9 +83,146 @@ class cscs_build_osu_benchmarks(build_osu_benchmarks):
                 ]
 
 
-class cscs_osu_benchmarks(osu_build_run, ExtraLauncherOptionsMixin):
-    osu_binaries = fixture(cscs_build_osu_benchmarks, scope='environment')
-    num_iters = 100
+    @run_before('compile')
+    def prepare_build(self):
+        tarball = f'osu-micro-benchmarks-{self.osu_benchmarks.version}.tar.gz'
+        self.build_prefix = tarball[:-7]  # remove .tar.gz extension
+        fullpath = os.path.join(self.osu_benchmarks.stagedir, tarball)
+        self.prebuild_cmds += [
+            f'cp {fullpath} {self.stagedir}',
+            f'tar xzf {tarball}',
+            f'cd {self.build_prefix}'
+        ]
+        if self.build_type != 'cpu':
+            self.build_system.config_opts = [f'--enable-{self.build_type}']
+
+        # Change to the c/mpi directory to avoid building the
+        # non-mpi benchmarks
+        self.build_system.make_opts = ['-C', 'c/mpi']
+        self.build_system.max_concurrency = 8
+
+    @sanity_function
+    def validate_build(self):
+        # If build fails, the test will fail before reaching this point.
+        return True
+
+
+class osu_benchmark(ExtraLauncherOptionsMixin, rfm.RunOnlyRegressionTest):
+    '''OSU benchmark test base class.'''
+
+    #: Number of warmup iterations.
+    #:
+    #: This value is passed to the excutable through the -x option.
+    #:
+    #: :type: :class:`int`
+    #: :default: ``10``
+    num_warmup_iters = variable(int, value=10)
+
+    #: Number of iterations.
+    #:
+    #: This value is passed to the excutable through the -i option.
+    #:
+    #: :type: :class:`int`
+    #: :default: ``100``
+    num_iters = variable(int, value=100)
+
+    #: Maximum message size.
+    #:
+    #: Both the performance and the sanity checks will be done
+    #: for this message size.
+    #:
+    #: This value is set to ``8`` for latency benchmarks and to ``4194304`` for
+    #: bandwidth benchmarks.
+    #:
+    #: :type: :class:`int`
+    message_size = variable(int)
+
+    #: Device buffers.
+    #:
+    #: Use accelerator device buffers.
+    #: Valid values are ``cpu``, ``cuda``, ``openacc`` or ``rocm``.
+    #:
+    #: :type: :class:`str`
+    #: :default: ``'cpu'``
+    device_buffers = variable(str, value='cpu')
+
+    #: Number of tasks to use.
+    #:
+    #: This variable is required.
+    #: It is set to ``2`` for point to point benchmarks, but it is undefined
+    #: for collective benchmarks
+    #:
+    #: :required: Yes
+    num_tasks = required
+    num_tasks_per_node = 1
+
+    #: Parameter indicating the available benchmark to execute.
+    #:
+    #: :type: 2-element tuple containing the benchmark name and whether latency
+    #:   or bandwidth is to be measured.
+    #:
+    #: :values:
+    #:   ``mpi.collective.blocking.osu_alltoall``,
+    #:   ``mpi.collective.blocking.osu_allreduce``,
+    #:   ``mpi.pt2pt.standard.osu_bw``,
+    #:   ``mpi.pt2pt.standard.osu_latency``
+    benchmark_info = parameter([
+        ('mpi.collective.blocking.osu_alltoall', 'latency'),
+        ('mpi.collective.blocking.osu_allreduce', 'latency'),
+        ('mpi.pt2pt.standard.osu_bw', 'bandwidth'),
+        ('mpi.pt2pt.standard.osu_latency', 'latency')
+    ], fmt=lambda x: x[0], loggable=True)
+
+    tags = {'cpe'}
+
+    @run_before('setup')
+    def setup_per_benchmark(self):
+        bench, bench_metric = self.benchmark_info
+        if bench_metric == 'latency':
+            self.message_size = 8
+            unit = 'us'
+        elif bench_metric == 'bandwidth':
+            self.message_size = 4194304
+            unit = 'MB/s'
+        else:
+            raise ValueError(f'unknown benchmark metric: {bench_metric}')
+
+        self.executable = bench.split('.')[-1]
+        self.executable_opts = ['-m', f'{self.message_size}',
+                                '-x', f'{self.num_warmup_iters}',
+                                '-i', f'{self.num_iters}', '-c']
+
+        if self.device_buffers != 'cpu':
+            self.executable_opts += ['-d', self.device_buffers]
+
+        if bench.startswith('mpi.pt2pt'):
+            self.executable_opts += ['D', 'D']
+            self.num_tasks = 2
+
+        self.perf_variables = {
+            bench_metric: sn.make_performance_function(
+                self._extract_metric, unit
+            )
+        }
+
+    @sanity_function
+    def validate_test(self):
+        return sn.assert_found(rf'^{self.message_size}.*Pass', self.stdout)
+
+    @deferrable
+    def _extract_metric(self):
+        return sn.extractsingle(rf'^{self.message_size}\s+(\S+)',
+                                self.stdout, 1, float)
+
+
+class osu_build_run(osu_benchmark):
+    '''OSU benchmark test (build and run)'''
+
+    #: The fixture object that builds the OSU binaries
+    #:
+    #: :type: :class:`build_osu_benchmarks`
+    #: :scope: *environment*
+    osu_binaries = fixture(build_osu_benchmarks, scope='environment')
 
     @run_after('init')
     def set_valid_systems_envs(self):
@@ -71,18 +246,22 @@ class cscs_osu_benchmarks(osu_build_run, ExtraLauncherOptionsMixin):
                 'CUDA_VISIBLE_DEVICES': 0,
             }
 
-    exclusive_access = True
-    tags = {'cpe'}
+    @run_before('run')
+    def prepend_build_prefix(self):
+        bench_path = self.benchmark_info[0].replace('.', '/')
+        self.executable = os.path.join(self.osu_binaries.stagedir,
+                                       self.osu_binaries.build_prefix,
+                                       'c', bench_path)
 
 
 @rfm.simple_test
-class cscs_osu_pt2pt_check(cscs_osu_benchmarks):
+class osu_pt2pt_check(osu_build_run):
     benchmark_info = parameter([
-        ('mpi.pt2pt.osu_bw', 'bandwidth'),
-        ('mpi.pt2pt.osu_latency', 'latency')
+        ('mpi.pt2pt.standard.osu_bw', 'bandwidth'),
+        ('mpi.pt2pt.standard.osu_latency', 'latency')
     ], fmt=lambda x: x[0], loggable=True)
     allref = {
-        'mpi.pt2pt.osu_bw': {
+        'mpi.pt2pt.standard.osu_bw': {
             'cpu': {
                 '*': {
                     'bandwidth': (24000.0, -0.10, None, 'MB/s')
@@ -94,7 +273,7 @@ class cscs_osu_pt2pt_check(cscs_osu_benchmarks):
                 }
             }
         },
-        'mpi.pt2pt.osu_latency': {
+        'mpi.pt2pt.standard.osu_latency': {
             'cpu': {
                 '*': {
                     'latency': (2.0, None, 0.15, 'us')
@@ -121,24 +300,24 @@ class cscs_osu_pt2pt_check(cscs_osu_benchmarks):
 
 
 @rfm.simple_test
-class cscs_osu_collective_check(cscs_osu_benchmarks):
+class osu_collective_check(osu_build_run):
     benchmark_info = parameter([
-        ('mpi.collective.osu_alltoall', 'latency'),
-        ('mpi.collective.osu_allreduce', 'latency'),
+        ('mpi.collective.blocking.osu_alltoall', 'latency'),
+        ('mpi.collective.blocking.osu_allreduce', 'latency'),
     ], fmt=lambda x: x[0], loggable=True)
 
     num_nodes = parameter([6])
-    osu_binaries = fixture(cscs_build_osu_benchmarks, scope='environment')
+    osu_binaries = fixture(build_osu_benchmarks, scope='environment')
 
     allref = {
-        'mpi.collective.osu_allreduce': {
+        'mpi.collective.blocking.osu_allreduce': {
             6: {
                 '*': {
                     'latency': (8.45, None, 0.10, 'us')
                 },
             },
         },
-        'mpi.collective.osu_alltoall': {
+        'mpi.collective.blocking.osu_alltoall': {
             6: {
                 '*': {
                     'latency': (14.50, None, 0.10, 'us')
