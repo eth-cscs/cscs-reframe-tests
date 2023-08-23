e4e88ffc854a6536fdb33b484c50ca2088a6391b
Theofilos Manitaras <manitaras@cscs.ch>
2023-08-23 16:05:24 +0200

    Add Cuda aware mpi test for uenv
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

---
+++ uenv_checks/prgenv/cuda_aware_mpi.py
@@ -0,0 +1,93 @@
+# Copyright 2016-2023 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import pathlib
+import sys
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+import reframe.utility as rfm_util
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent / 'mixins'))
+
+from cuda_visible_devices_all import CudaVisibleDevicesAllMixin
+
+
+@rfm.simple_test
+class cuda_aware_mpi_check(rfm.CompileOnlyRegressionTest):
+    descr = 'Cuda-aware MPI test from NVIDIA code-samples.git'
+    sourcesdir = ('https://github.com/NVIDIA-developer-blog/'
+                  'code-samples.git')
+    valid_systems = ['+remote +nvgpu']
+    valid_prog_environs = ['+cuda +mpi']
+    build_locally = False
+    prebuild_cmds = ['cd posts/cuda-aware-mpi-example/src']
+    build_system = 'Make'
+    postbuild_cmds = ['ls ../bin']
+
+    @run_before('compile')
+    def set_compilers(self):
+        # Remove the `sm_` prefix from the cuda arch
+        gpu_arch = self.current_partition.select_devices('gpu')[0].arch[3:]
+        gcd_flgs = f'-gencode arch=compute_{gpu_arch},code=sm_{gpu_arch}'
+        self.build_system.options = [
+            f'CUDA_INSTALL_PATH=$CUDA_HOME',  # cuda_runtime.h
+            f'GENCODE_FLAGS="{gcd_flgs}"',
+            f'MPICC="{self.current_environ.cc}"',
+            f'MPILD="{self.current_environ.cxx}"'
+        ]
+
+    @run_before('sanity')
+    def set_sanity_patterns(self):
+        self.sanity_patterns = sn.assert_found(r'jacobi_cuda_aware_mpi',
+                                               self.stdout)
+
+
+class CudaAwareMpiRuns(CudaVisibleDevicesAllMixin, rfm.RunOnlyRegressionTest):
+    valid_systems = ['+remote +nvgpu']
+    valid_prog_environs = ['+cuda +mpi']
+    env_vars = {
+        'MPICH_GPU_SUPPORT_ENABLED': 1,
+        'LD_LIBRARY_PATH': '${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}'
+    }
+
+    @run_after('init')
+    def add_deps(self):
+        self.depends_on('cuda_aware_mpi_check')
+
+    @require_deps
+    def set_executable(self, cuda_aware_mpi_check):
+        self.executable = str(
+            pathlib.Path(cuda_aware_mpi_check().stagedir) / 'posts' /
+            'cuda-aware-mpi-example' / 'bin' / 'jacobi_cuda_aware_mpi'
+        )
+
+    @run_before('sanity')
+    def set_sanity_patterns(self):
+        self.sanity_patterns = sn.assert_found(r'Stopped after 1000 iterations'
+                                               r' with residue 0.00024',
+                                               self.stdout)
+
+@rfm.simple_test
+class cuda_aware_mpi_one_node_check(CudaAwareMpiRuns):
+    # Enable Cuda MPS to use more tasks than the available gpus
+    prerun_cmds += ['nvidia-cuda-mps-control -d ']
+    postrun_cmds += ['echo quit | nvidia-cuda-mps-control']
+
+    @run_before('run')
+    def set_num_tasks(self):
+        self.num_tasks = 2 * self.num_gpus_per_node
+        self.num_tasks_per_node = self.num_tasks
+        self.executable_opts = [f'-t {self.num_tasks // 2} 2']
+
+
+@rfm.simple_test
+class cuda_aware_mpi_two_nodes_check(CudaAwareMpiRuns):
+    @run_before('run')
+    def set_num_tasks(self):
+        self.num_tasks = 2
+        self.num_tasks_per_node = 1
+        self.num_gpus_per_node = 1
+        self.executable_opts = [f'-t {self.num_tasks} 1']
