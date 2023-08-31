f8193dca927d4554066ff054df67cc1b4a92cdd3
jgp <jgphpc@users.noreply.github.com>
2023-08-31 08:40:59 +0200

    Add Cuda nvml check for uenv (#105)
    * nvml

--- uenv_checks/prgenv/cuda_nvml.py
+++ uenv_checks/prgenv/cuda_nvml.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# Copyright 2016-2023 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
 # ReFrame Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -8,38 +8,36 @@ import reframe.utility.sanity as sn
 
 
 @rfm.simple_test
-class NvmlCheck(rfm.RegressionTest):
+class NvmlTest(rfm.RegressionTest):
     descr = 'Checks that nvml can report GPU informations'
     valid_systems = ['+nvgpu']
     valid_prog_environs = ['+cuda']
-    modules = ['cudatoolkit']
     build_system = 'SingleSource'
-    sourcepath = '$CUDATOOLKIT_HOME/nvml/example/example.c'
-    maintainers = []
+    sourcesdir = None
+    sourcepath = '$CUDA_HOME/nvml/example/example.c'
+    build_locally = False
     tags = {'production', 'craype', 'external-resources', 'health'}
 
     @run_before('compile')
-    def setup_nvhpc(self):
-        if 'PrgEnv-nvhpc' == self.current_environ.name:
-            self.modules.remove('cudatoolkit')
-            self.sourcepath = '$NVIDIA_PATH/cuda/bin/../nvml/example/example.c'
-
-    @run_before('compile')
     def set_build_flags(self):
-        self.build_system.ldflags = ['-lnvidia-ml']
+        self.prebuild_cmds = [f'echo "# {self.sourcepath}"']
+        self.build_system.cflags = ['-I $CUDA_HOME/include']
+        self.build_system.ldflags = ['-L $CUDA_HOME/lib64 -lnvidia-ml']
 
-    @run_before('sanity')
-    def set_sanity_patterns(self):
+    @sanity_function
+    def set_sanity(self):
         """
         Listing devices:
         0. NVIDIA A100-SXM4-80GB [00000000:03:00.0]
         Changing device's compute mode from 'Exclusive Process' to 'Prohibited'
         Need root privileges to do that: Insufficient Permissions # <-- OK
         """
-        regex_devices = 'Found \d+ devices'
-        regex_pciinfo = '\d+. NVIDIA.*\[\S+\]'
+        cp = self.current_partition
+        self.gpu_count = cp.select_devices('gpu')[0].num_devices
+        regex_devices = rf'Found {self.gpu_count} devices'
+        regex_pciinfo = r'\d+. NVIDIA.*\[\S+\]'
         regex_mode = 'Need root privileges to do that: Insufficient Permission'
-        self.sanity_patterns = sn.all([
+        return sn.all([
             sn.assert_found(regex_devices, self.stdout),
             sn.assert_found(regex_pciinfo, self.stdout),
             sn.assert_found(regex_mode, self.stdout),
