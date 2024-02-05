762565c73273f80837f6d8b8eac881614bfea18f
Henrique <henrique.mendonca@cscs.ch>
2024-02-05 18:10:12 +0100

    CE pytorch ddp

--- checks/apps/pytorch/pytorch_distr_check.py
+++ checks/apps/pytorch/pytorch_distr_check.py
@@ -1,45 +1,39 @@
 import os
+import sys
+import pathlib
 import reframe as rfm
 import reframe.utility.sanity as sn
 
+sys.path.append(str(pathlib.Path(__file__).parent.parent.parent / 'mixins'))
+from container_engine import ContainerEngineMixin  # noqa: E402
 
-@rfm.simple_test
+
 class pytorch_distr_cnn(rfm.RunOnlyRegressionTest):
     descr = 'Check the training throughput of a cnn with torch.distributed'
-    platform = parameter(['native', 'Sarus'])
-    valid_systems = ['hohgant:nvgpu']
+    valid_systems = ['+nvgpu']
     valid_prog_environs = ['builtin']
+    num_nodes = parameter([1, 2, 4, 8])
     sourcesdir = 'src'
-    num_tasks = 16
-    num_tasks_per_node = 4
-    num_gpus_per_node = 4
-    throughput_per_gpu = 864.62
+    throughput_per_gpu = 800
     executable = 'python cnn_distr.py'
-    throughput_total = throughput_per_gpu * num_tasks
-    reference = {
-        'hohgant:nvgpu': {
-            'samples_per_sec_per_gpu': (throughput_per_gpu,
-                                        -0.1, None, 'samples/sec'),
-            'samples_per_sec_total': (throughput_total,
-                                      -0.1, None, 'samples/sec')
-        }
-    }
     tags = {'production'}
 
-    @run_after('init')
-    def skip_native_test(self):
-        # FIXME: Remove this when PyTorch is available on Hohgant
-        self.modules = ['cray', 'cray-python']
-        self.prerun_cmds = [
-            '. /apps/hohgant/sarafael/deeplearning-env/bin/activate'
-        ]
-
-    @run_before('run')
-    def set_container_variables(self):
-        if self.platform != 'native':
-            self.container_platform = self.platform
-            self.container_platform.command = self.executable
-            self.container_platform.image = 'nvcr.io/nvidia/pytorch:22.08-py3'
+    @run_after('setup')
+    def set_num_gpus_per_node(self):
+        curr_part = self.current_partition
+        self.num_gpus_per_node = curr_part.select_devices('gpu')[0].num_devices
+        self.num_tasks_per_node = self.num_gpus_per_node
+        self.num_tasks = self.num_nodes * self.num_tasks_per_node
+        self.throughput_total = self.throughput_per_gpu * self.num_tasks
+        self.reference = {
+            '*': {
+                'samples_per_sec_per_gpu': (self.throughput_per_gpu,
+                                            -0.1, None, 'samples/sec'),
+                'samples_per_sec_total': (self.throughput_total,
+                                        -0.1, None, 'samples/sec')
+            }
+        }
+        self.job.launcher.options += ['-l --gpus-per-task=1']
 
     @sanity_function
     def assert_job_is_complete(self):
@@ -59,9 +53,21 @@ class pytorch_distr_cnn(rfm.RunOnlyRegressionTest):
             self.stdout, 'samples_per_sec_total', float
         ))
 
-    @run_before('run')
-    def set_visible_devices_per_rank(self):
-        if self.platform != 'native':
-            self.job.launcher.options == ['--mpi=pmi2']
 
-        self.job.launcher.options.append('./set_visible_devices.sh')
+@rfm.simple_test
+class PyTorchDdpCE(pytorch_distr_cnn, ContainerEngineMixin):
+    valid_systems = ['+ce +nvgpu']
+    container_image = 'nvcr.io#nvidia/pytorch:24.01-py3'
+
+
+@rfm.simple_test
+class PyTorchDdpSarus(pytorch_distr_cnn):
+    valid_systems = ['+nvgpu']
+    platform = 'Sarus'
+
+    @run_before('run')
+    def set_container_variables(self):
+        self.container_platform = self.platform
+        self.container_platform.command = self.executable
+        self.container_platform.image = 'nvcr.io/nvidia/pytorch:22.12-py3'  # cuda11.8
+        self.job.launcher.options.append('--mpi=pmi2')
--- checks/apps/pytorch/src/set_visible_devices.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IFS=$'\n'
-export CUDA_VISIBLE_DEVICES=$SLURM_LOCALID
-exec $*
