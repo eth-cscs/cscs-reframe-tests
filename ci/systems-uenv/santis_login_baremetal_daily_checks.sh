183a275cb8c70aa15e5baccbd5df4fea8aaa2ddc
Jonathan Coles <jonathan.coles@cscs.ch>
2024-02-14 19:45:55 +0100

    .

--- ci/systems-uenv/santis_login_baremetal_daily_checks.sh
+++ ci/systems-uenv/santis_login_baremetal_daily_checks.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
-echo "hello world!"
-
-exit
-
-rm -fr venv_reframe_cpe
-python3 -m venv venv_reframe_cpe
-source venv_reframe_cpe/bin/activate
+rm -fr venv_reframe
+python3 -m venv venv_reframe
+source venv_reframe/bin/activate
 pip install reframe-hpc
 
-#reframe -c config/cscs.py ${changedtests_path} --report-junit=$REPORT -r
+reframe -c config/cscs.py -c checks/prgenv/mpi.py --report-junit=$REPORT -r
 
 deactivate
 
