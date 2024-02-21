adf6369d37753bb0a4cce12c0767ad74a160bf9f
Henrique <henrique.mendonca@cscs.ch>
2024-02-21 17:30:27 +0100

    rollback to install path

--- checks/apps/pytorch/src/setup_conda.sh
+++ checks/apps/pytorch/src/setup_conda.sh
@@ -5,4 +5,4 @@ INSTALL_PATH=${1:-$PWD/forge}
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh -b -p $INSTALL_PATH
 . $INSTALL_PATH/etc/profile.d/conda.sh
-export PATH=$CONDA_PREFIX/bin:$PATH
+export PATH=$INSTALL_PATH/bin:$PATH
