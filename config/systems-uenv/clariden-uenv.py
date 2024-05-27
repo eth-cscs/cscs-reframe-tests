115d0cf23481d132ead6588c7c72405661a5c3c3
jgp <jgphpc@users.noreply.github.com>
2024-05-27 14:14:03 +0200

    ci for uenv testing on santis (#188)
    CI for uenv testing on santis & todi
    
    ---------
    
    Signed-off-by: Theofilos Manitaras <manitart@cscs.ch>
    Co-authored-by: Theofilos Manitaras <manitart@cscs.ch>

--- config/systems-uenv/clariden-uenv.py
+++ config/systems-uenv/clariden-uenv.py
@@ -42,7 +42,7 @@ try:
         image_environments = yaml.load(
             image_envs.read(), Loader=yaml.BaseLoader)
 except OSError as err:
-    raise ConfigError(f"problem loading the metadata from '{rfm_meta}'")
+    raise ConfigError(f"CLA: problem loading the metadata from '{rfm_meta}'")
 
 
 environs = image_environments.keys()
