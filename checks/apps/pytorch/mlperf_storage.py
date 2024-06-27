f08ef6d8bbb5276aee4bfb6de66d930ac8ec3dfd
Theofilos Manitaras <manitaras@cscs.ch>
2024-06-27 16:35:16 +0200

    Refactor using fixtures
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

--- checks/apps/pytorch/mlperf_storage.py
+++ checks/apps/pytorch/mlperf_storage.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+import os
 import sys
 import pathlib
 
@@ -13,14 +14,16 @@ sys.path.append(str(pathlib.Path(__file__).parent.parent.parent / 'mixins'))
 from container_engine import ContainerEngineMixin  # noqa: E402
 
 
-class MLperfStorageBase(rfm.RunOnlyRegressionTest):
+class mlperf_storage_datagen_sarus(rfm.RunOnlyRegressionTest):
     image = 'jfrog.svc.cscs.ch/reframe-oci/mlperf-storage:v1.0-mpi'
+    valid_systems = ['+sarus']
     valid_prog_environs = ['builtin']
     num_nodes = parameter([1, 2])
     time_limit = '20m'
     accelerator_type = 'h100'
     workload = 'unet3d'
     tags = {'production'}
+    platform = 'Sarus'
     env_vars = {
         'LC_ALL': 'C',
         'HYDRA_FULL_ERROR': '1',
@@ -30,29 +33,64 @@ class MLperfStorageBase(rfm.RunOnlyRegressionTest):
     @run_after('setup')
     def setup_test(self):
         curr_part = self.current_partition
-        self.num_gpus_per_node = curr_part.select_devices('gpu')[0].num_devices
+        self.num_gpus_per_node = 1 # curr_part.select_devices('gpu')[0].num_devices
         self.num_tasks_per_node = self.num_gpus_per_node
         self.num_tasks = self.num_nodes * self.num_tasks_per_node
         self.num_files = 512 * self.num_tasks
        
         self.executable = rf""" bash -c '
-            set -xe;
-
             ./benchmark.sh datagen --workload {self.workload} \
                 --accelerator-type {self.accelerator_type} \
                 --num-parallel {self.num_tasks} \
                 --param dataset.num_files_train={self.num_files} \
-                --param dataset.data_folder=/rfm_workdir/unet3d_data;
+                --param dataset.data_folder=/rfm_workdir/dataset;
+        ' """
 
+    @run_before('run')
+    def set_container_variables(self):
+        self.container_platform = self.platform
+        self.container_platform.command = self.executable
+        self.container_platform.image = self.image
+        self.container_platform.workdir = None
+        self.job.launcher.options = ['--mpi=pmi2']
+
+    @sanity_function
+    def assert_data_generation_done(self):
+        return sn.assert_found(r'.*Generation done.*', self.stderr)
+
+
+@rfm.simple_test
+class MLperfStorageSarus(rfm.RunOnlyRegressionTest):
+    descr = 'Check the training throughput using Sarus and NVIDIA NGC'
+    valid_systems = ['+sarus']
+    valid_prog_environs = ['builtin']
+    time_limit = '20m'
+    tags = {'production'}
+    env_vars = {
+        'LC_ALL': 'C',
+        'HYDRA_FULL_ERROR': '1',
+        'RDMAV_FORK_SAFE': '1',
+    }
+    mlperf_data = fixture(mlperf_storage_datagen_sarus, scope='environment')
+    
+    @run_after('setup')
+    def setup_test(self):
+        curr_part = self.current_partition
+        self.num_gpus_per_node =  self.mlperf_data.num_gpus_per_node
+        self.num_tasks_per_node = self.num_gpus_per_node
+        self.num_tasks = self.mlperf_data.num_nodes * self.num_tasks_per_node
+        num_files = 512 * self.num_tasks
+        self.workload = self.mlperf_data.workload
+        accelerator_type = self.mlperf_data.accelerator_type
+       
+        self.executable = rf""" bash -c '
             ./benchmark.sh run --workload {self.workload} \
-                --accelerator-type {self.accelerator_type} \
+                --accelerator-type {accelerator_type} \
                 --num-accelerators {self.num_tasks} \
                 --results-dir resultsdir \
-                --param dataset.num_files_train={self.num_files} \
-                --param dataset.data_folder=/rfm_workdir/unet3d_data;
+                --param dataset.num_files_train={num_files} \
+                --param dataset.data_folder=/dataset;
         ' """
-        # clean up data
-        self.postrun_cmds = ['rm -rf /rfm_workdir/unet3d_data']
 
         self.reference = {
             '*': {
@@ -60,6 +98,17 @@ class MLperfStorageBase(rfm.RunOnlyRegressionTest):
             }
         }
         self.job.launcher.options += ['-l']
+
+    @run_before('run')
+    def set_container_variables(self):
+        self.container_platform = self.mlperf_data.platform
+        self.container_platform.command = self.executable
+        self.container_platform.image = self.mlperf_data.image
+        self.container_platform.mount_points = [
+            (os.path.join(self.mlperf_data.stagedir, 'dataset'), '/dataset')
+        ]
+        self.job.launcher.options = ['--mpi=pmi2']
+        self.container_platform.workdir = None
 
     @sanity_function
     def assert_job_is_complete(self):
@@ -71,49 +120,3 @@ class MLperfStorageBase(rfm.RunOnlyRegressionTest):
             r'Training I/O Throughput \(MB/second\): (?P<mbs_total>\S+)',
             self.stderr, 'mbs_total', float
         ))
-
-
-
-@rfm.simple_test
-class MLperfStorageBaseCEtest(MLperfStorageBase, ContainerEngineMixin):
-    descr = ('Check the training throughput using the ContainerEngine and '
-             'NVIDIA NGC')
-    valid_systems = ['+ce']
-    tags = {'production', 'ce'}
-    sourcesdir = 'src'
-    
-    @run_before('run')
-    def set_container_variables(self):
-        self.container_image = self.image
-
-
-@rfm.simple_test
-class MLperfStorageBaseSarusTest(MLperfStorageBase):
-    descr = 'Check the training throughput using Sarus and NVIDIA NGC'
-    valid_systems = ['+nvgpu +sarus']
-    platform = 'Sarus'
-
-    @run_before('run')
-    def set_container_variables(self):
-        self.container_platform = self.platform
-        self.container_platform.command = self.executable
-        self.container_platform.image = self.image
-        self.container_platform.workdir = None
-        self.container_platform.with_mpi = True
-
-
-@rfm.simple_test
-class MLperfStorageBaseDockerTest(MLperfStorageBase):
-    descr = 'Check the training throughput using Docker and NVIDIA NGC'
-    valid_systems = ['+docker']
-    platform = 'Docker'
-    num_nodes = parameter([1])
-    num_files = 16
-
-    @run_before('run')
-    def set_container_variables(self):
-        self.container_platform = self.platform
-        self.container_platform.command = self.executable
-        self.container_platform.image = self.image
-        self.container_platform.workdir = None
-        self.container_platform.options = ['-e RDMAV_FORK_SAFE=1']
