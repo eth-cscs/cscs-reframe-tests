e5adf84ca95e1d01e04007200b20eb12901f9239
Jonathan Coles <jonathan.coles@cscs.ch>
2024-07-26 13:43:29 +0200

    Integration tests: Allow restrictions to both system and features.

--- checks/system/integration/utils.py
+++ checks/system/integration/utils.py
@@ -4,6 +4,9 @@
 #                                                                             #
 # ----------------------------------------------------------------------------#
 
+from inspect import getframeinfo, stack
+
+
 class Check:
 
     CLASS = ''
@@ -27,6 +30,10 @@ class Check:
         :param not_expected: Similar to 'expected', but what should not be found.
         """
 
+        def debuginfo():
+            caller = getframeinfo(stack()[2][0])
+            return "[%s:%d]" % (caller.filename, caller.lineno)
+
         check_count[0] += 1
 
         lcl_CHECK_CLASS = ''
@@ -41,16 +48,18 @@ class Check:
                 if self.CLASS not in self.CLASS_INCLUDE:
                     return
 
-        if where and where[0] not in ['-', '+']:
+        if not where:
+            where = '*'
+        elif where[0] not in ['-', '+']:
             where = f'+{where}'
 
         name                = f'Check_{check_count[0]:04}{lcl_CHECK_CLASS}'
-        valid_systems       = [self.SYSTEM] + where.split()
+        valid_systems       = where.split()
         valid_prog_environs = ['builtin']
         time_limit          = '2m'
 
         if self.DEBUG:
-            print(   f"{name:25} {cmd:80} -> "
+            print(   f"{debuginfo()} {name:25} {cmd:80} -> "
                   + (f"expected:{expected}"           if expected     is not None else '') 
                   + (f"not_expected:{not_expected}"   if not_expected is not None else '') 
                   + (f"  on  {valid_systems} with {valid_prog_environs}"))
@@ -62,28 +71,39 @@ class Check:
         from reframe.core.meta import make_test
 
 
+
         def validate(test):
-            def v0(ex):
-                nonlocal test
-                e,w = ex if isinstance(ex, list) else [ex, 'stdout']
-                assert w in ['stdout', 'stderr']
-                return [e, eval(f'test.{w}')]
 
             a,b = True,True
-            expected, where = v0(test.expected)
-            if expected is not None:
-                msg = f"Expected '{expected}' running '{test.cmd}'"
-                a = sn.assert_found(expected, where, msg=msg)
 
-            not_expected, where = v0(test.not_expected)
+            expected, where = test.expected
+            if expected is not None:
+                where = eval(f'test.{test.expected[1]}')
+                a = sn.assert_found(expected, where, 
+                                    msg=f"Expected '{expected}' running '{test.cmd} {test.caller}'")
+
+            not_expected, where = test.not_expected
             if not_expected is not None:
-                msg = f"Did not expect '{not_expected}' running '{test.cmd}'"
-                b = sn.assert_not_found(not_expected, where, msg=msg)
+                where = eval(f'test.{test.not_expected[1]}')
+                b = sn.assert_not_found(not_expected, where, 
+                                        msg=f"Did not expect '{not_expected}' running '{test.cmd} {test.caller}'")
             return (a and b) 
 
         def set_executable_opts(test):
             test.executable = test.cmd
-            #test.executable, *test.executable_opts = test.cmd.split()
+
+            test.expected     =     test.expected if isinstance(    test.expected, list) else [    test.expected, 'stdout']
+            test.not_expected = test.not_expected if isinstance(test.not_expected, list) else [test.not_expected, 'stdout']
+
+            test.skip_if(    test.expected[1] not in ['stdout', 'stderr'], msg=f'Location for expected is not stdout or stderr {test.caller}')
+            test.skip_if(test.not_expected[1] not in ['stdout', 'stderr'], msg=f'Location for not_expected is not stdout or stderr {test.caller}')
+
+
+        def check_system(test):
+            test.skip_if(test.current_system.name != self.SYSTEM,
+                         msg = f'Required sytem {self.SYSTEM} but found {test.current_system.name}.')
+
+
 
         t = make_test(
                 f'Check_{check_count[0]:04}{lcl_CHECK_CLASS}', 
@@ -94,8 +114,10 @@ class Check:
                  'valid_systems': valid_systems,
                  'valid_prog_environs': valid_prog_environs,
                  'time_limit': time_limit,
+                 'caller': debuginfo(),
                 },
-                [builtins.run_before('run')(set_executable_opts),
+                [builtins.run_after('setup')(set_executable_opts),
+                 builtins.run_after('setup')(check_system),
                  builtins.sanity_function(validate),
                 ]
             )
