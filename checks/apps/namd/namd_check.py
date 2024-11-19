962e73779b30b0cf17ab52c6d81602fc6edc00c0
Rocco Meli <rocco.meli@cscs.ch>
2024-11-19 17:45:30 +0100

    ''''''''''''''''''''''''''''''''''''''''''''''''

--- checks/apps/namd/namd_check.py
+++ checks/apps/namd/namd_check.py
@@ -12,38 +12,38 @@ import reframe.utility.udeps as udeps
 import uenv
 
 namd_references = {
-    "stmv": {"gh200": {"ns_day": (93, -0.05, None, "ns/day")}},
+    'stmv': {'gh200': {'ns_day': (93, -0.05, None, 'ns/day')}},
 }
 
 slurm_config = {
-    "stmv": {
-        "gh200": {
-            "nodes": 1,
-            "ntasks-per-node": 1,
-            "cpus-per-task": 29,
-            "walltime": "0d0h5m0s",
-            "gpu": True,
+    'stmv': {
+        'gh200': {
+            'nodes': 1,
+            'ntasks-per-node': 1,
+            'cpus-per-task': 29,
+            'walltime': '0d0h5m0s',
+            'gpu': True,
         }
     }
 }
 
 
 class namd_download(rfm.RunOnlyRegressionTest):
-    """
+    '''
     Download NAMD source code.
-    """
+    '''
 
-    version = variable(str, value="3.0")
-    descr = "Fetch NAMD source code"
+    version = variable(str, value='3.0')
+    descr = 'Fetch NAMD source code'
     sourcedir = None
-    executable = "curl"
+    executable = 'curl'
     executable_opts = [
-        "-f",  # Try to have curl not return 0 on server error
-        "-u",
-        "${CSCS_REGISTRY_USERNAME}:${CSCS_REGISTRY_PASSWORD}",
-        f"https://jfrog.svc.cscs.ch/artifactory/uenv-sources/namd/NAMD_{version}_Source.tar.gz",
-        "--output",
-        f"NAMD_{version}_Source.tar.gz",
+        '-f',  # Try to have curl not return 0 on server error
+        '-u',
+        '${CSCS_REGISTRY_USERNAME}:${CSCS_REGISTRY_PASSWORD}',
+        f'https://jfrog.svc.cscs.ch/artifactory/uenv-sources/namd/NAMD_{version}_Source.tar.gz',
+        '--output',
+        f'NAMD_{version}_Source.tar.gz',
     ]
     local = True
 
@@ -53,10 +53,10 @@ class namd_download(rfm.RunOnlyRegressionTest):
 
 
 class AutotoolsCustom(rfm.core.buildsystems.Autotools):
-    """
+    '''
     Custom build system deriving from autotools.
     NAMD uses a ./config script instead of ./configure
-    """
+    '''
 
     def __init__(self):
         super().__init__()
@@ -64,26 +64,26 @@ class AutotoolsCustom(rfm.core.buildsystems.Autotools):
     def emit_build_commands(self, environ):
         prepare_cmd = []
         if self.srcdir:
-            prepare_cmd += [f"cd {self.srcdir}"]
+            prepare_cmd += [f'cd {self.srcdir}']
 
         if self.builddir:
-            prepare_cmd += [f"mkdir -p {self.builddir}", f"cd {self.builddir}"]
+            prepare_cmd += [f'mkdir -p {self.builddir}', f'cd {self.builddir}']
 
         if self.builddir:
             configure_cmd = [
                 os.path.join(
-                    os.path.relpath(self.configuredir, self.builddir), "config"
+                    os.path.relpath(self.configuredir, self.builddir), 'config'
                 )
             ]
         else:
-            configure_cmd = [os.path.join(self.configuredir, "config")]
+            configure_cmd = [os.path.join(self.configuredir, 'config')]
 
         if self.config_opts:
             configure_cmd += self.config_opts
 
-        config_dir = "Linux-ARM64-g++.cuda"
+        config_dir = 'Linux-ARM64-g++.cuda'
 
-        make_cmd = ["make -j"]
+        make_cmd = ['make -j']
         if self.max_concurrency is not None:
             make_cmd += [str(self.max_concurrency)]
 
@@ -91,115 +91,115 @@ class AutotoolsCustom(rfm.core.buildsystems.Autotools):
             make_cmd += self.make_opts
 
         return prepare_cmd + [
-            " ".join(configure_cmd),  # Run ./config
-            f"cd {config_dir}",  # cd to directory created by .config
-            " ".join(make_cmd),  # make
+            ' '.join(configure_cmd),  # Run ./config
+            f'cd {config_dir}',  # cd to directory created by .config
+            ' '.join(make_cmd),  # make
         ]
 
 
 @rfm.simple_test
 class NamdBuildTest(rfm.CompileOnlyRegressionTest):
-    """
+    '''
     Test NAMD build from source.
-    """
+    '''
 
-    descr = "NAMD Build Test"
-    valid_prog_environs = ["+namd-single-node-dev"]
-    valid_systems = ["*"]
+    descr = 'NAMD Build Test'
+    valid_prog_environs = ['+namd-single-node-dev']
+    valid_systems = ['*']
     build_system = AutotoolsCustom()
     sourcesdir = None
-    maintainers = ["SSA"]
-    namd_sources = fixture(namd_download, scope="session")
+    maintainers = ['SSA']
+    namd_sources = fixture(namd_download, scope='session')
     build_locally = False
-    tags = {"uenv"}
+    tags = {'uenv'}
 
-    @run_before("compile")
+    @run_before('compile')
     def prepare_build(self):
         self.uarch = uenv.uarch(self.current_partition)
         self.build_system.builddir = self.stagedir
         self.skip_if_no_procinfo()
         cpu = self.current_partition.processor
-        self.build_system.max_concurrency = cpu.info["num_cpus_per_socket"]
+        self.build_system.max_concurrency = cpu.info['num_cpus_per_socket']
 
         tarsource = os.path.join(
             self.namd_sources.stagedir,
-            f"NAMD_{self.namd_sources.version}_Source.tar.gz",
+            f'NAMD_{self.namd_sources.version}_Source.tar.gz',
         )
 
         # Extract source code and compiler bundled Charm++
         self.prebuild_cmds = [
-            f"tar --strip-components=1 -xzf {tarsource} -C {self.stagedir}",
-            "tar -xf charm-8.0.0.tar",  # INFO: Depends on the NAMD version
-            "cd charm-8.0.0",
-            f"./build charm++ multicore-linux-arm8 gcc --with-production --enable-tracing -j {self.build_system.max_concurrency}",
-            "cd ..",
+            f'tar --strip-components=1 -xzf {tarsource} -C {self.stagedir}',
+            'tar -xf charm-8.0.0.tar',  # INFO: Depends on the NAMD version
+            'cd charm-8.0.0',
+            f'./build charm++ multicore-linux-arm8 gcc --with-production --enable-tracing -j {self.build_system.max_concurrency}',
+            'cd ..',
             # Link against tcl8.6 (provided by the UENV)
-            "sed -i 's/-ltcl8.5/-ltcl8.6/g' arch/Linux-ARM64.tcl",
+            'sed -i \'s/-ltcl8.5/-ltcl8.6/g\' arch/Linux-ARM64.tcl',
         ]
 
-        prefix = "/user-environment/env/develop-single-node"
+        prefix = '/user-environment/env/develop-single-node'
 
         self.build_system.config_opts = [
-            "Linux-ARM64-g++.cuda",
-            "--charm-arch multicore-linux-arm8-gcc",
-            f"--charm-base {self.stagedir}/charm-8.0.0",
-            "--with-tcl",
-            f"--tcl-prefix {prefix}",
-            "--with-fftw",
-            "--with-fftw3",
-            f"--fftw-prefix {prefix}",
+            'Linux-ARM64-g++.cuda',
+            '--charm-arch multicore-linux-arm8-gcc',
+            f'--charm-base {self.stagedir}/charm-8.0.0',
+            '--with-tcl',
+            f'--tcl-prefix {prefix}',
+            '--with-fftw',
+            '--with-fftw3',
+            f'--fftw-prefix {prefix}',
         ]
 
-        if self.uarch == "gh200":
+        if self.uarch == 'gh200':
             self.build_system.config_opts += [
-                "--with-single-node-cuda",
-                "--with-cuda",
-                "--cuda-gencode arch=compute_90,code=sm_90",
+                '--with-single-node-cuda',
+                '--with-cuda',
+                '--cuda-gencode arch=compute_90,code=sm_90',
             ]
 
-        self.extra_resources = {"reservation": {"reservation": "NCCL"}}
+        self.extra_resources = {'reservation': {'reservation': 'NCCL'}}
 
     @sanity_function
     def validate_test(self):
         self.namd_executable = os.path.join(
-            self.stagedir, "Linux-ARM64-g++.cuda", "namd3"
+            self.stagedir, 'Linux-ARM64-g++.cuda', 'namd3'
         )
         return os.path.isfile(self.namd_executable)
 
 
 class NamdCheck(rfm.RunOnlyRegressionTest):
-    descr = "NAMD STMV Benchmark"
-    test_name = "stmv"
-    valid_systems = ["*"]
-    executable = "namd3"
-    maintainers = ["SSA"]
+    descr = 'NAMD STMV Benchmark'
+    test_name = 'stmv'
+    valid_systems = ['*']
+    executable = 'namd3'
+    maintainers = ['SSA']
 
-    @run_before("run")
+    @run_before('run')
     def prepare_run(self):
         self.uarch = uenv.uarch(self.current_partition)
         config = slurm_config[self.test_name][self.uarch]
 
-        self.num_tasks_per_node = config["ntasks-per-node"]
-        self.num_tasks = config["nodes"] * self.num_tasks_per_node
-        self.num_cpus_per_task = config["cpus-per-task"]
+        self.num_tasks_per_node = config['ntasks-per-node']
+        self.num_tasks = config['nodes'] * self.num_tasks_per_node
+        self.num_cpus_per_task = config['cpus-per-task']
         self.ntask_per_core = 1
-        self.time_limit = config["walltime"]
+        self.time_limit = config['walltime']
 
         self.executable_opts = [
-            f"+p {self.num_cpus_per_task}",
-            "+pmepes 5",
-            "+setcpuaffinity",
+            f'+p {self.num_cpus_per_task}',
+            '+pmepes 5',
+            '+setcpuaffinity',
         ]
 
-        if self.uarch == "gh200":
-            self.executable_opts += ["+devices 0,1,2,3"]
+        if self.uarch == 'gh200':
+            self.executable_opts += ['+devices 0,1,2,3']
             # It is required to make all the GPUs visible
             self.extra_resources = {
-                "gres": {"gres": "gpu:4"},
-                "reservation": {"reservation": "NCCL"},
+                'gres': {'gres': 'gpu:4'},
+                'reservation': {'reservation': 'NCCL'},
             }
 
-        self.executable_opts += ["stmv_gpures_nve.namd"]
+        self.executable_opts += ['stmv_gpures_nve.namd']
 
         if self.uarch is not None and self.uarch in namd_references[self.test_name]:
             self.reference = {
@@ -212,9 +212,9 @@ class NamdCheck(rfm.RunOnlyRegressionTest):
     def validate_energy(self):
         energy = sn.avg(
             sn.extractall(
-                r"ENERGY:([ \t]+\S+){10}[ \t]+(?P<energy>\S+)",
+                r'ENERGY:([ \t]+\S+){10}[ \t]+(?P<energy>\S+)',
                 self.stdout,
-                "energy",
+                'energy',
                 float,
             )
         )
@@ -227,7 +227,7 @@ class NamdCheck(rfm.RunOnlyRegressionTest):
                 sn.assert_eq(
                     sn.count(
                         sn.extractall(
-                            r"TIMING: (?P<step_num>\S+)  CPU:", self.stdout, "step_num"
+                            r'TIMING: (?P<step_num>\S+)  CPU:', self.stdout, 'step_num'
                         )
                     ),
                     25,  # 500 steps and output frequency of 20
@@ -236,13 +236,13 @@ class NamdCheck(rfm.RunOnlyRegressionTest):
             ]
         )
 
-    @performance_function("ns/day")
+    @performance_function('ns/day')
     def ns_day(self):
         return sn.avg(
             sn.extractall(
-                r"PERFORMANCE:.* averaging (?P<ns_day>\S+) ns/day",
+                r'PERFORMANCE:.* averaging (?P<ns_day>\S+) ns/day',
                 self.stdout,
-                "ns_day",
+                'ns_day',
                 float,
             )
         )
@@ -250,20 +250,20 @@ class NamdCheck(rfm.RunOnlyRegressionTest):
 
 @rfm.simple_test
 class NamdCheckUenvExec(NamdCheck):
-    valid_prog_environs = ["+namd-single-node"]
-    tags = {"uenv", "production"}
+    valid_prog_environs = ['+namd-single-node']
+    tags = {'uenv', 'production'}
 
 
 @rfm.simple_test
 class NamdCheckCustomExec(NamdCheck):
-    valid_prog_environs = ["+namd-single-node-dev"]
-    tags = {"uenv"}
+    valid_prog_environs = ['+namd-single-node-dev']
+    tags = {'uenv'}
 
-    @run_after("init")
+    @run_after('init')
     def setup_dependency(self):
-        self.depends_on("NamdBuildTest", udeps.fully)
+        self.depends_on('NamdBuildTest', udeps.fully)
 
-    @run_after("setup")
+    @run_after('setup')
     def setup_executable(self):
-        parent = self.getdep("NamdBuildTest")
-        self.executable = f"{parent.namd_executable}"
+        parent = self.getdep('NamdBuildTest')
+        self.executable = f'{parent.namd_executable}'
