ca16eaef15c9dfea172137d7c5607416f324cca7
Rocco Meli <rocco.meli@cscs.ch>
2024-11-29 08:44:15 +0100

    remove reservation from todi

--- config/systems/todi.py
+++ config/systems/todi.py
@@ -66,10 +66,6 @@ base_config = {
                     'name': 'memory',
                     'options': ['--mem={mem_per_node}']
                 },
-                {
-                    'name': 'reservation',
-                    'options': ['--reservation={reservation}']
-                },
             ],
             'devices': [
                 {
