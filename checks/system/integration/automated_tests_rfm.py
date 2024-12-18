495b559b9d5b7e02ca0bcdc13de34d1cd8674609
Blanca Fuentes <blafuemon@gmail.com>
2024-12-18 14:41:04 +0100

    Add v-cluster tests

--- checks/system/integration/automated_tests_rfm.py
+++ checks/system/integration/automated_tests_rfm.py
@@ -3,7 +3,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-import yaml
 from utils import *
 from constants import *
 import os
@@ -14,21 +13,21 @@ import json
 
 # --------------------------------------------------------------------------- #
 #                READ THE CONFIGURATION FROM THE YAML FILES
-#                This part should be modified for the final 
+#                This part should be modified for the final
 #                structure of the V-Clusters config files
-# --------------------------------------------------------------------------- 
+# ---------------------------------------------------------------------------
 
-system_data_file = "eiger_data.json"
+system_data_file = "daint_data.json"
 
 # Read the extracted info from the json file
 with open(os.path.join(json_file_path, system_data_file), 'r') as json_file:
     config_yaml_data = json.load(json_file)
 
 # Check for mount points to be checked
-mount_info = config_yaml_data.get(MOUNT_VARS)
+mount_info = config_yaml_data.get(MOUNT_VARS[0])
 
 # Check for pkg installations to be checked
-tools_info = config_yaml_data.get(TOOLS_VARS)
+tools_info = config_yaml_data.get("pkgs")
 
 # Check for environment variables to be checked
 envs_info = config_yaml_data.get(ENV_VARS)
@@ -37,23 +36,33 @@ envs_info = config_yaml_data.get(ENV_VARS)
 proxy_info = {}
 if config_yaml_data.get(PROXY_VARS['proxy_server']) and config_yaml_data.get(PROXY_VARS['proxy_port']):
     proxy_info.update({"http_proxy": f"{config_yaml_data.get(PROXY_VARS['proxy_server'])}" +
-                            f":{config_yaml_data.get(PROXY_VARS['proxy_port'])}",
-                        "https_proxy": f"{config_yaml_data.get(PROXY_VARS['proxy_server'])}" +
-                            f":{config_yaml_data.get(PROXY_VARS['proxy_port'])}"})
+                       f":{config_yaml_data.get(PROXY_VARS['proxy_port'])}",
+                       "https_proxy": f"{config_yaml_data.get(PROXY_VARS['proxy_server'])}" +
+                       f":{config_yaml_data.get(PROXY_VARS['proxy_port'])}"})
 if config_yaml_data.get(PROXY_VARS['no_proxy']):
-    proxy_info.update({"no_proxy":   ", ".join(config_yaml_data.get(PROXY_VARS['no_proxy']))})
+    proxy_info.update({"no_proxy":   ", ".join(
+        config_yaml_data.get(PROXY_VARS['no_proxy']))})
 
 # --------------------------------------------------------------------------- #
 #                PERFORM INTEGRATION CHECKS
 # --------------------------------------------------------------------------- #
 
 @rfm.simple_test
-class MountTest(rfm.RunOnlyRegressionTest):
+class MountPointExistsTest(rfm.RunOnlyRegressionTest):
 
     if mount_info:
         mount_info_par = []
         for mount_i in mount_info:
-            mount_info_par.append((mount_i['mount_point'], mount_i['fstype']))
+            if mount_i.get('fstype'):
+                mount_info_par.append(
+                    (mount_i['mount_point'],
+                     mount_i['fstype'])
+                )
+            else:
+                mount_info_par.append(
+                    (mount_i['mount_point'],
+                     None)
+                )
         mount_info = parameter(mount_info_par)
         # For now
         valid_systems = ['-remote']
@@ -62,20 +71,24 @@ class MountTest(rfm.RunOnlyRegressionTest):
     descr = 'Test mount points in the system'
     valid_prog_environs = ['builtin']
     time_limit = '2m'
-    tags = {'MOUNT'}
+    tags = {'MOUNT', 'VS-NODE-VALIDATOR'}
 
     @run_after('setup')
     def set_executable(self):
-        self.executable = f'grep -q "{self.mount_info[0]} {self.mount_info[1]}" /proc/mounts || echo FAILED'
+        if self.mount_info[1]:
+            self.executable = f'grep -q "{self.mount_info[0]} {self.mount_info[1]}" /proc/mounts || echo FAILED'
+        else:
+            self.executable = f'ls {self.mount_info[0]} || echo FAILED'
 
     @sanity_function
     def validate(self):
-        return sn.assert_not_found('FAILED', self.stdout, 
-                msg=f'Mount point "{self.mount_info[0]} {self.mount_info[1]}" not found in /proc/mounts')
+
+        return sn.assert_not_found('FAILED', self.stdout,
+                                   msg=f'Mount point "{self.mount_info[0]} {self.mount_info[1]}" not found in /proc/mounts')
 
 
 @rfm.simple_test
-class ToolsTest(rfm.RunOnlyRegressionTest):
+class PackagePresentTest(rfm.RunOnlyRegressionTest):
 
     if tools_info:
         tools_info = parameter(tools_info)
@@ -86,19 +99,26 @@ class ToolsTest(rfm.RunOnlyRegressionTest):
     descr = 'Test pkgs installation in the system'
     valid_prog_environs = ['builtin']
     time_limit = '2m'
-    tags = {'TOOLS'}
+    tags = {'TOOLS', 'VS-NODE-VALIDATOR'}
 
     @run_after('setup')
     def set_executable(self):
-        self.executable = f'which {pkg_names[self.tools_info]} || echo FAILED'
+        self.executable = ''
+        for pkg in pkg_names[self.tools_info]:
+            if not self.executable:
+                self.executable += f'(which {pkg}'
+            else:
+                self.executable += f' && which {pkg}'
+        self.executable += ') || echo FAILED'
 
     @sanity_function
     def validate(self):
-        return sn.assert_not_found('FAILED', self.stdout, 
-                msg=f'Did not find an installation for {self.tools_info}')
+        return sn.assert_not_found('FAILED', self.stdout,
+                                   msg=f'Did not find an installation for {self.tools_info}')
+
 
 @rfm.simple_test
-class EnvTest(rfm.RunOnlyRegressionTest):
+class EnvVariableConfigTest(rfm.RunOnlyRegressionTest):
 
     if envs_info:
         envs_info_par = []
@@ -120,14 +140,15 @@ class EnvTest(rfm.RunOnlyRegressionTest):
 
     @sanity_function
     def validate(self):
-        return sn.assert_found(self.envs_info[1], self.stdout, 
-                msg=f'Environment variable {self.envs_info[0]} is not set up correctly')
+        return sn.assert_found(self.envs_info[1], self.stdout,
+                               msg=f'Environment variable {self.envs_info[0]} is not set up correctly')
 
 
 @rfm.simple_test
-class ProxyTest(rfm.RunOnlyRegressionTest):
+class ProxyConfigTest(rfm.RunOnlyRegressionTest):
 
-    proxy_info_par = [(k, v) for k, v in proxy_info.items() if v and "None" not in v ]
+    proxy_info_par = [(k, v)
+                      for k, v in proxy_info.items() if v and "None" not in v]
     if proxy_info_par:
         proxy_info = parameter(proxy_info_par)
         valid_systems = ['+remote']
@@ -144,5 +165,5 @@ class ProxyTest(rfm.RunOnlyRegressionTest):
 
     @sanity_function
     def validate(self):
-        return sn.assert_found(self.proxy_info[1], self.stdout, 
-                msg=f'Proxy configuration {self.proxy_info[0]} is not set up correctly')
+        return sn.assert_found(self.proxy_info[1], self.stdout,
+                               msg=f'Proxy configuration {self.proxy_info[0]} is not set up correctly')
--- checks/system/integration/constants.py
+++ checks/system/integration/constants.py
@@ -5,23 +5,48 @@
 #
 
 # Package names
-pkg_names = {"bubblewrap"             : "bwrap",
-             "ltrace"                 : "ltrace",
-             "gcc12"                  : "gcc-12",
-             "nnn"                    : "nnn",
-             "netcat"                 : "nc",
-             "lua-lmod"               : "lua",
-             "pycxi-utils"            : "cxi_stat",
-             "gcc12-fortran"          : "gfortran-12",
-             "datacenter-gpu-manager" : "dcgmi",
-             "ansible"                : "ansible",
-             "gcc12-c++"              : "g++-12"}
+pkg_names = {'bubblewrap': ['bwrap'],
+             'ltrace': ['ltrace'],
+             'gcc12': ['gcc-12'],
+             'nnn': ['nnn'],
+             'netcat': ['netcat'],
+             'lua-lmod': ['lua'],
+             'pycxi-utils': ['cxi_stat', 'cxi_atomic_bw',
+                             'cxi_atomic_lat', 'cxi_gpu_loopback_bw',
+                             'cxi_healthcheck', 'cxi_heatsink_check',
+                             'cxi_read_bw', 'cxi_read_lat',
+                             'cxi_send_bw', 'cxi_send_lat',
+                             'cxi_write_lat', 'cxi_write_bw'],
+             'gcc12-fortran': ['gfortran-12'],
+             'datacenter-gpu-manager': ['dcgmi'],
+             'ansible': ['ansible'],
+             'gcc12-c++': ['g++-12'],
+             'fuse3': ['mount', 'adduser', 'libc6', 'libfuse3-3',
+                       'sed'],
+             'libtool': ['libtool'],
+             'wget': ['wget'],
+             'htop': ['htop'],
+             'lsof': ['lsof'],
+             'lbcd': ['lbcdclient']
+             }
 
-json_file_path = "../systems_data"
+tools_installation = {'fakerootuidsync': '/usr/sbin/fakerootuidsync',
+                      }
 
-MOUNT_VARS   = "filesystems"
-TOOLS_VARS   = "cscs_cluster_extra_pkgs"
-ENV_VARS     = "cscs_extra_vars"
-PROXY_VARS   = {"proxy_server": "proxy_server",
-                "proxy_port": "proxy_port",
-                "no_proxy": "no_proxy"}
+json_file_path = '../systems_data'
+
+MOUNT_VARS   = ['filesystems',
+                'capstor_scratch_cscs_path',
+                'capstor_store_cscs_path',
+                'capstor_users_cscs_path',
+                'iopsstor_scratch_cscs_path',
+                'iopsstor_store_cscs_path']
+
+TOOLS_VARS   = ['cscs_cluster_extra_pkgs', 'cscs_platform_packages']
+ENV_VARS     = 'cscs_extra_vars'
+PROXY_VARS   = {'proxy_server': 'proxy_server',
+                'proxy_port': 'proxy_port',
+                'no_proxy': 'no_proxy'}
+
+VSERVICES_DICT = {'v-cluster': 'vcluster',
+                  'storage': 'storage'}
