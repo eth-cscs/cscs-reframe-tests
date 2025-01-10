289c6319324bac11a9a8702b7a09750577da9806
Theofilos Manitaras <manitart@cscs.ch>
2025-01-10 13:20:17 +0100

    Update clariden config to reflect new Vcluster
    Signed-off-by: Theofilos Manitaras <manitart@cscs.ch>

--- config/systems/clariden.py
+++ config/systems/clariden.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2023 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# Copyright 2024 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
 # ReFrame Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -6,186 +6,85 @@
 # ReFrame CSCS settings
 #
 
+import reframe.utility.osext as osext
+import copy
+
+
+base_config = {
+    'modules_system': 'nomod',
+    'partitions': [
+        {
+            'name': 'login',
+            'scheduler': 'local',
+            'time_limit': '10m',
+            'environs': [
+                'builtin',
+            ],
+            'descr': 'Login nodes',
+            'max_jobs': 4,
+            'launcher': 'local'
+        },
+        {
+            'name': 'normal',
+            'descr': 'GH200',
+            'scheduler': 'slurm',
+            'time_limit': '10m',
+            'container_platforms': [
+            ],
+            'environs': [
+                'builtin',
+            ],
+            'max_jobs': 100,
+            'extras': {
+                'cn_memory': 825,
+            },
+            'features': ['ce', 'gpu', 'nvgpu', 'remote', 'scontrol', 'uenv'],
+            'resources': [
+                {
+                    'name': 'switches',
+                    'options': ['--switches={num_switches}']
+                },
+                {
+                    'name': 'gres',
+                    'options': ['--gres={gres}']
+                },
+                {
+                    'name': 'memory',
+                    'options': ['--mem={mem_per_node}']
+                },
+            ],
+            'devices': [
+                {
+                    'type': 'gpu',
+                    'arch': 'sm_90',
+                    'num_devices': 4
+                }
+                ],
+            'launcher': 'srun',
+        },
+    ]
+}
+
+base_config['name'] = 'clariden'
+base_config['descr'] = 'Clariden vcluster'
+base_config['hostnames'] = ['clariden']
+
 site_configuration = {
     'systems': [
-        {
-            'name': 'clariden',
-            'descr': 'Clariden vcluster',
-            'hostnames': ['clariden'],
-            'modules_system': 'lmod',
-            'partitions': [
-                {
-                    'name': 'login',
-                    'scheduler': 'local',
-                    'time_limit': '10m',
-                    'environs': [
-                        'builtin',
-                        'PrgEnv-cray',
-                        'PrgEnv-gnu',
-                        'PrgEnv-nvhpc',
-                        'PrgEnv-nvidia'
-                    ],
-                    'features': [
-                        'enroot'
-                    ],
-                    'descr': 'Login nodes',
-                    'max_jobs': 4,
-                    'launcher': 'local'
-                },
-                {
-                    'name': 'nvgpu',
-                    'scheduler': 'slurm',
-                    'time_limit': '10m',
-                    'environs': [
-                        'builtin',
-                        'PrgEnv-cray',
-                        'PrgEnv-gnu',
-                        'PrgEnv-nvhpc',
-                        'PrgEnv-nvidia'
-                    ],
-                    'max_jobs': 100,
-                    'extras': {
-                        'cn_memory': 500,
-                    },
-                    'access': ['-pnvgpu'],
-                    'resources': [
-                        {
-                            'name': 'switches',
-                            'options': ['--switches={num_switches}']
-                        },
-                        {
-                            'name': 'memory',
-                            'options': ['--mem={mem_per_node}']
-                        },
-                    ],
-                    'features': [
-                        'gpu', 'nvgpu', 'remote', 'enroot', 'ce', 'buildah', 'scontrol'
-                    ],
-                    'devices': [
-                        {
-                            'type': 'gpu',
-                            'arch': 'sm_80',
-                            'num_devices': 4
-                        }
-                    ],
-                    'launcher': 'srun'
-                },
-                {
-                    'name': 'amdgpu',
-                    'scheduler': 'slurm',
-                    'time_limit': '10m',
-                    'environs': [
-                        'builtin',
-                        'PrgEnv-cray',
-                        'PrgEnv-gnu'
-                    ],
-                    'max_jobs': 100,
-                    'extras': {
-                        'cn_memory': 500,
-                    },
-                    'access': ['-pamdgpu'],
-                    'resources': [
-                        {
-                            'name': 'switches',
-                            'options': ['--switches={num_switches}']
-                        },
-                        {
-                            'name': 'memory',
-                            'options': ['--mem={mem_per_node}']
-                        },
-                    ],
-                    'features': [
-                        'gpu', 'amdgpu', 'remote', 'enroot', 'ce', 'buildah', 'scontrol'
-                    ],
-                    'devices': [
-                        {
-                            'type': 'gpu',
-                            'arch': 'gfx90a',
-                            'num_devices': 8
-                        }
-                    ],
-                    'launcher': 'srun'
-                },
-            ]
-        },
+        base_config,
     ],
     'environments': [
-        {
-            'name': 'PrgEnv-cray',
-            'target_systems': ['clariden'],
-            'modules': ['cray', 'PrgEnv-cray'],
-            'features': ['serial', 'openmp', 'mpi', 'cuda', 'openacc', 'hdf5',
-                         'netcdf-hdf5parallel', 'pnetcdf', 'openmp', 'opencl'],
-            'extras': {
-                'c_openmp_flags': ['-fopenmp'],
-                'f_openmp_flags': ['-homp']
-            }
-        },
-        {
-            'name': 'PrgEnv-gnu',
-            'target_systems': ['clariden'],
-            'modules': ['cray', 'PrgEnv-gnu'],
-            'features': ['serial', 'openmp', 'mpi', 'cuda', 'alloc_speed',
-                         'hdf5', 'netcdf-hdf5parallel', 'pnetcdf', 'openmp'],
-            'extras': {
-                'hugepages2M': ['craype-hugepages2M'],
-                'c_openmp_flags': ['-fopenmp']
-            }
-        },
-        {
-            'name': 'PrgEnv-nvhpc',
-            'target_systems': ['clariden'],
-            'modules': ['cray', 'PrgEnv-nvhpc'],
-            'features': ['serial', 'openmp', 'mpi', 'cuda',
-                         'hdf5', 'netcdf-hdf5parallel', 'pnetcdf', 'openmp'],
-            'extras': {
-                'launcher_options': ['--mpi=pmi2'],
-                'c_openmp_flags': ['-fopenmp']
-            },
-        },
-        {
-            'name': 'PrgEnv-nvidia',
-            'target_systems': ['clariden'],
-            'modules': ['cray', 'PrgEnv-nvidia'],
-            'features': ['serial', 'openmp', 'mpi', 'cuda-fortran', 'openacc',
-                         'netcdf-hdf5parallel', 'pnetcdf', 'openmp', 'opencl'
-                         'hdf5'],
-            'extras': {
-                # Workaround "MPIR_pmi_init(83)....: PMI2_Job_GetId returned 14" error
-                'launcher_options': ['--mpi=pmi2'],
-                'c_openmp_flags': ['-mp']
-            },
-        },
     ],
     'modes': [
-        {
-            'name': 'production',
-            'options': [
-                '--unload-module=reframe',
-                '--exec-policy=async',
-                '-Sstrict_check=1',
-                '--prefix=$SCRATCH/regression/production',
-                '--report-file=$SCRATCH/regression/production/reports/prod_report_{sessionid}.json',
-                '--save-log-files',
-                '--tag=production',
-                '--timestamp=%F_%H-%M-%S'
-            ],
-            'target_systems': ['clariden'],
-        },
-        {
-            'name': 'production-ce',
-            'options': [
-                '--unload-module=reframe',
-                '--exec-policy=async',
-                '-Sstrict_check=1',
-                '--prefix=$SCRATCH/regression/production',
-                '--report-file=$SCRATCH/regression/production/reports/prod_report_{sessionid}.json',
-                '--save-log-files',
-                '--tag=\'^production$\'',
-                '--tag=\'^ce$\'',
-                '--timestamp=%F_%H-%M-%S'
-            ],
-            'target_systems': ['clariden'],
-        }
-    ]
+       {
+           'name': 'uenv_production',
+           'options': [
+               '--max-retries=1',
+               '--report-file=$PWD/latest.json',
+               '-c checks/apps',
+               '--tag=production'
+           ],
+           'target_systems': ['clariden'],
+       }
+   ]
 }
