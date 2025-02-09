08fd8f76423d0c35e334f81930650eadee303c63
jg <jg>
2025-02-09 13:56:31 +0100

    278654

--- ci/scripts/alps.sh
+++ ci/scripts/alps.sh
@@ -266,6 +266,7 @@ install_reframe() {
     rm -fr rfm_venv reframe
     python3 -m venv rfm_venv
     source rfm_venv/bin/activate
+    pip install -U pip
     # pip install --upgrade reframe-hpc
     # git clone --depth 1 https://github.com/reframe-hpc/reframe.git
     # multi-uenv support only in reframe > v4.5.2:
@@ -274,7 +275,8 @@ install_reframe() {
     export PATH="$(pwd)/reframe-develop/bin:$PATH"
     echo "$(pwd)/reframe-develop/bin"
     # deps for cscs-reframe-tests.git:
-    pip install python-hostlist requests toml &> .deps.cscs-reframe-tests
+    pip install -r config/utilities/requirements.txt # pyfirecrest + python-hostlist
+    pip install requests toml &> .deps.cscs-reframe-tests
     # (wget --quiet "https://github.com/reframe-hpc/reframe/archive/refs/tags/v4.5.2.tar.gz" && \
     # tar xf v4.5.2.tar.gz && \
     # cd reframe-4.5.2 && \
@@ -332,8 +334,10 @@ launch_reframe_1arg() {
     export RFM_AUTODETECT_METHODS="cat /etc/xthostname,hostname"
     export RFM_USE_LOGIN_SHELL=1
     # export RFM_AUTODETECT_XTHOSTNAME=1
-    # reframe -V
-    echo "UENV=$UENV"
+    source rfm_venv/bin/activate
+    reframe -V
+    echo "# UENV=$UENV"
+    echo "# CPE=$CPE"
     echo "# img=$img"
     reframe -C ./config/cscs.py \
         --report-junit=report.xml \
