b2c21396830b997b1685c6c661e9b3e2866d5371
Jean-guillaume Piccinali <jgphpc@users.noreply.github.com>
2025-02-18 13:01:37 +0100

    Update gromacs_check.py

--- checks/apps/gromacs/gromacs_check.py
+++ checks/apps/gromacs/gromacs_check.py
@@ -77,7 +77,6 @@ class gromacs_build_test(rfm.CompileOnlyRegressionTest):
     @run_before('compile')
     def prepare_build(self):
         self.uarch = uarch(self.current_partition)
-        # self.build_system.builddir = os.path.join(self.stagedir, 'build')
         self.skip_if_no_procinfo()
         cpu = self.current_partition.processor
         self.build_system.max_concurrency = cpu.info['num_cpus_per_socket']
@@ -100,15 +99,16 @@ class gromacs_build_test(rfm.CompileOnlyRegressionTest):
 
     @sanity_function
     def validate_test(self):
-        folder = 'bin' # add folder path
         self.gromacs_executable = os.path.join('bin', 'gmx_mpi')
-        print(self.gromacs_executable)
         return os.path.isfile(self.gromacs_executable)
+
 
 @rfm.simple_test
 class gromacs_run_test(rfm.RunOnlyRegressionTest):
     executable = './mps-wrapper.sh -- gmx_mpi mdrun -s topol.tpr'
-    executable_opts = ['-dlb no', '-ntomp 32', '-pme gpu', '-npme 1', '-bonded gpu', '-nb gpu', '-nsteps 10000', '-update gpu', '-pin off', '-v', '-noconfout', '-nstlist 300']
+    executable_opts = ['-dlb no', '-ntomp 32', '-pme gpu', '-npme 1', '-bonded
+                       gpu', '-nb gpu', '-nsteps 10000', '-update gpu', '-pin
+                       off', '-v', '-noconfout', '-nstlist 300']
     maintainers = ['SSA']
     valid_systems = ['*']
     test_name = variable(str, value='STMV')
@@ -118,7 +118,6 @@ class gromacs_run_test(rfm.RunOnlyRegressionTest):
     def prepare_run(self):
         self.uarch = uarch(self.current_partition)
         config = slurm_config[self.test_name][self.uarch]
-        # sbatch options
         self.job.options = [
             f'--nodes={config["nodes"]}',
         ]
@@ -126,13 +125,10 @@ class gromacs_run_test(rfm.RunOnlyRegressionTest):
         self.num_tasks = config['nodes'] * self.num_tasks_per_node
         self.num_cpus_per_task = config['cpus-per-task']
         self.time_limit = config['walltime']
-
         # environment variables
         self.env_vars['OMP_NUM_THREADS'] = str(self.num_cpus_per_task)
         self.env_vars['FI_CXI_RX_MATCH_MODE'] = 'software'
-
         if self.uarch == 'gh200':
-
             self.env_vars['MPICH_GPU_SUPPORT_ENABLED'] = '1'
             self.env_vars['GMX_GPU_DD_COMMS'] = 'true'
             self.env_vars['GMX_GPU_PME_PP_COMMS'] = 'true'
@@ -146,21 +142,24 @@ class gromacs_run_test(rfm.RunOnlyRegressionTest):
                 self.current_partition.fullname:
                     gromacs_references[self.test_name][self.uarch]
             }
-        
+
         # set input files
         if self.test_name == 'STMV':
-            self.prerun_cmds = ['wget https://jfrog.svc.cscs.ch/artifactory/cscs-reframe-tests/gromacs/STMV/topol.tpr --quiet']
-
+            jfrog = 'https://jfrog.svc.cscs.ch/artifactory/cscs-reframe-tests/'
+            self.prerun_cmds = [f'wget {jfrog}/gromacs/STMV/topol.tpr --quiet']
 
     @run_after('setup')
     def postproc_run(self):
-        # extract Bond Energy from the binary .edr file and write it to a readable .xvg file and then write it to stdout
+        """
+        Extract Bond Energy from the binary .edr file and write it to a
+        readable .xvg file and then write it to stdout
+        """
         self.postrun_cmds = [
             'echo -e "1\\n" |'
             'gmx_mpi energy -f ener.edr -o ener.xvg',
             'cat ener.xvg'
         ]
-        
+
     @sanity_function
     def validate_job(self):
         regex1 = r'^Bond\s+\S+\s+'
@@ -170,18 +169,6 @@ class gromacs_run_test(rfm.RunOnlyRegressionTest):
             sn.assert_found(regex2, self.stderr, msg='regex2 failed'),
         ])
         return self.sanity_patterns
-    
-    # TODO find fix for the sanity check for Bond Energy
-    # @performance_function('bond_energy')
-    # def validate_bond_energy(self):
-    #     # RegEx to extract from ener.xvg
-    #     regex = r'^Bond\s+(?P<bond_energy>\S+)\s+'
-    #     energy = sn.extractsingle(regex, self.stdout, 'bond_energy', int)
-
-    #     energy_reference = 164780
-    #     energy_diff = sn.abs(energy - energy_reference)
-
-    #     sn.assert_lt(energy_diff, 8000, msg=f'tolerance limit exceeded {energy_diff} ') #~5% tolerance
 
     @performance_function('ns/day')
     def perf(self):
