1fe7b49419ab1c49810729de5e373a7a6333112d
Theofilos Manitaras <manitart@cscs.ch>
2025-03-18 11:31:14 +0100

    Create mode for cpe_ce
    Signed-off-by: Theofilos Manitaras <manitart@cscs.ch>

--- config/systems/daint.py
+++ config/systems/daint.py
@@ -164,7 +164,19 @@ site_configuration = {
                '--max-retries=1',
                '--report-file=$PWD/latest.json',
                '-c checks',
-               '--tag=production'
+               '--tag=production',
+               '-p \'(?!PrgEnv-ce)\''
+           ],
+           'target_systems': ['daint'],
+       },
+       {
+           'name': 'cpe_ce_production',
+           'options': [
+               '--max-retries=1',
+               '--report-file=$PWD/latest.json',
+               '-c checks',
+               '--tag=production',
+               '-p PrgEnv-ce'
            ],
            'target_systems': ['daint'],
        },
