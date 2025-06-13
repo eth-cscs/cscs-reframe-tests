421588f1b052610c970a2c4d993e18a74dd19439
Theofilos Manitaras <manitaras@cscs.ch>
2025-06-13 14:15:02 +0200

    Upgrade node burn image and zen2 perf ref, add alps-eiger config (#338)
    Signed-off-by: Theofilos Manitaras <manitart@cscs.ch>
    Co-authored-by: Theofilos Manitaras <manitart@cscs.ch>

---
+++ config/systems/alps-eiger.py
@@ -0,0 +1,71 @@
+# Copyright 2025 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# ReFrame CSCS settings
+#
+
+
+import reframe.utility.osext as osext
+
+
+site_configuration = {
+    'systems': [
+        {
+            'name': 'alps-eiger',
+            'descr': 'Alps Eiger vcluster',
+            'hostnames': ['alps-eiger'],
+            'modules_system': 'nomod',
+            'partitions': [
+                {
+                    'name': 'login',
+                    'scheduler': 'local',
+                    'time_limit': '10m',
+                    'environs': [
+                        'builtin',
+                    ],
+                    'descr': 'Login nodes',
+                    'max_jobs': 4,
+                    'launcher': 'local'
+                },
+                {
+                    'name': 'mc',
+                    'scheduler': 'slurm',
+                    'time_limit': '10m',
+                    'environs': [
+                        'builtin',
+                    ],
+                    'max_jobs': 100,
+                    'extras': {
+                        'cn_memory': 256,
+                    },
+                    'resources': [
+                        {
+                            'name': 'memory',
+                            'options': ['--mem={mem_per_node}']
+                        },
+                    ],
+                    'access': ['-Cmc', f'--account={osext.osgroup()}'],
+                    'features': ['ce', 'remote', 'scontrol', 'uenv'],
+                    'launcher': 'srun'
+                },
+            ]
+        },
+    ],
+    'modes': [
+        {
+            'name': 'production',
+            'options': [
+                '--exec-policy=async',
+                '-Sstrict_check=1',
+                '--prefix=$SCRATCH/regression/production',
+                '--report-file=$SCRATCH/regression/production/reports/prod_report_{sessionid}.json',
+                '--save-log-files',
+                '--tag=production',
+                '--timestamp=%F_%H-%M-%S'
+            ],
+            'target_systems': ['alps-eiger'],
+        }
+    ]
+}
