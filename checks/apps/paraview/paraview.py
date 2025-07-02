273a95785b82a26d812bab6e8dc33a218f22ec53
Jg_Piccinali <>
2025-07-02 18:59:09 +0200

    catalyst

--- checks/apps/paraview/paraview.py
+++ checks/apps/paraview/paraview.py
@@ -55,3 +55,62 @@ class ParaView_coloredSphere(rfm.RunOnlyRegressionTest):
             sn.assert_found(regex_render[arch], self.stdout),
             sn.assert_found(regex_png, self.stdout),
         ])
+
+
+@rfm.simple_test
+class ParaView_catalystClipping(rfm.RegressionTest):
+    # TODO: fix mpicc on eiger ?
+    valid_systems = ['+uenv +remote']
+    valid_prog_environs = ['+paraview-python']
+    git_tag = variable(str, value='0.1')
+    sourcesdir = None
+    build_system = 'CMake'
+    build_locally = False
+    num_tasks = 1
+    num_tasks_per_node = 1
+    time_limit = '3m'
+    executable = './bin/dummysph_catalystV2'
+    env_vars = {
+        'CATALYST_IMPLEMENTATION_NAME': 'paraview',
+        'CATALYST_IMPLEMENTATION_PATHS':
+        '/user-environment/ParaView-5.13/lib64/catalyst',
+        'CATALYST_DATA_DUMP_DIRECTORY': '$PWD/dataset',
+    }
+    maintainers = ['SSA']
+    tags = {'production'}
+
+    @run_before('compile')
+    def prepare_build(self):
+        tgz = f'v{self.git_tag}.tar.gz'
+        self.prebuild_cmds = [
+            f'# tested with paraview/5.13.2:v2 -v paraview-python',
+            f'wget -q https://github.com/jfavre/DummySPH/archive/refs/tags/'
+            f'{tgz} && tar xf {tgz} && rm -f {tgz}',
+        ]
+        self.build_system.config_opts = [
+            f'-S DummySPH-{self.git_tag}/src',
+            '-DINSITU=Catalyst'
+        ]
+
+    @run_before('run')
+    def prepare_runtime(self):
+        self.executable_opts = [
+            '--catalyst',
+            f'DummySPH-{self.git_tag}/ParaView_scripts/catalyst_clipping.py']
+        self.postrun_cmds = [
+            'head -1 datasets/dataset_000020/dataset_000020_0.vtp',
+            'head -1 datasets/dataset_000020.vtpd',
+            'file datasets/RenderView1_000020.png'
+        ]
+
+    @sanity_function
+    def assert_runtime(self):
+        regex_vtp = r'VTKFile type=\"PolyData\"'
+        regex_vtpd = r'VTKFile type=\"vtkPartitionedDataSet\"'
+        regex_png = r'PNG image data,'  # 1024 x 1024, 8-bit/color RGB
+
+        return sn.all([
+            sn.assert_found(regex_vtp, self.stdout),
+            sn.assert_found(regex_vtpd, self.stdout),
+            sn.assert_found(regex_png, self.stdout),
+        ])
