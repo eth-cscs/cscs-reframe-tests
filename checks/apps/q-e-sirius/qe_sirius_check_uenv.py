61c4eaa90f6617864891a564fe35f12af983b747
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-07-08 09:12:35 +0200

    update timings

--- checks/apps/q-e-sirius/qe_sirius_check_uenv.py
+++ checks/apps/q-e-sirius/qe_sirius_check_uenv.py
@@ -14,7 +14,7 @@ import uenv
 
 qe_references = {
     "Au surf": {
-        "gh200": {"time_run": (56, None, 0.05, "s")},
+        "gh200": {"time_run": (192, None, 0.05, "s")},
         "zen2": {"time_run": (99.45, None, 0.05, "s")},  # 1m44s
     },
 }
@@ -60,7 +60,7 @@ class QeCheckUENV(rfm.RunOnlyRegressionTest):
         self.time_limit = config["walltime"]
 
         # environment variables
-        self.env_vars["OMP_NUM_THREADS"] = str(1)
+        self.env_vars["OMP_NUM_THREADS"] = config["cpus-per-task"]
         self.env_vars["SLURM_HINT"] = "nomultithread"
         if self.uarch == "gh200":
             self.env_vars["MPICH_GPU_SUPPORT_ENABLED"] = "1"
