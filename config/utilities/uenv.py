f7a03092953411bf708974651e763ed25ce03d45
Jg_Piccinali <>
2025-09-29 18:48:47 +0200

    Check that uenv was pulled

--- config/utilities/uenv.py
+++ config/utilities/uenv.py
@@ -78,6 +78,8 @@ def _get_uenvs():
                 f"{inspect_cmd}='{{system}}'", shell=True).stdout.strip()
 
             image_path = pathlib.Path(image_path)
+            if not(image_path.is_file() and image_path.stat().st_size > 0):
+                raise ConfigError(f"{uenv_name} was not pulled or is empty")
 
             # FIXME temporary workaround for older uenv versions
             if Version(uenv_version) >= Version('5.1.0-dev'):
