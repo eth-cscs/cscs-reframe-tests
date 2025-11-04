23012ecb0cf7d59df70c871c023d9b6c48f557f5
Christos Kotsalos <christos.kotsalos@cscs.ch>
2025-11-04 15:49:33 +0100

    ICON4Py checks: WIP

--- checks/microbenchmarks/gpu/gpu_benchmarks/icon4py.py
+++ checks/microbenchmarks/gpu/gpu_benchmarks/icon4py.py
@@ -10,32 +10,34 @@ from uenv import uarch
 
 
 @rfm.simple_test
-class ICON4PyBenchmarks(rfm.RegressionTest):
+class ICON4PyBenchmarks(rfm.RunOnlyRegressionTest):
     descr = 'ICON4Py GPU benchmarks'
     maintainers = ['SSA']
     valid_systems = ['+uenv +amdgpu', '+uenv +nvgpu']
     valid_prog_environs = ['+uenv +prgenv +rocm', '+uenv +prgenv +cuda']
     sourcesdir = None
-    prebuild_cmds = [
+    prerun_cmds = [
+        'python -m venv .venv',
+        'source .venv/bin/activate',
         'git clone --depth 1 -b main https://github.com/C2SM/icon4py.git',
         'cd icon4py',
-        'curl -LsSf https://astral.sh/uv/install.sh | sh ',
+        'pip install --upgrade pip',
+        'pip install uv',
+        'rm uv.lock',
+        'export CC=$(which gcc) MPICH_CC=$(which gcc) CXX=$(which g++) MPICH_CXX=$(which g++)',
+        'uv sync --extra all --python $(which python) --active',
+        'uv pip uninstall mpi4py && uv pip install --no-binary mpi4py mpi4py',
+        'export CUPY_INSTALL_USE_HIP=1 HCC_AMDGPU_TARGET=gfx942 ROCM_HOME=/user-environment/env/default',
+        'uv pip install git+https://github.com/cupy/cupy.git',
     ]
-    time_limit = '2m'
+    time_limit = '20m'
     build_locally = False
     tags = {'production', 'uenv'}
-    executable = 'nox'
-
-    @run_before('compile')
-    def activate_venv(self):
-        self.prebuild_cmds += [
-            'uv sync --extra all',
-            'source ./venv/bin/activate'
-        ]
-
-    @run_before('run')
-    def activate_venv(self):
-        self.prerun_cmds += [
-            'nox -s __bencher_baseline_CI-3.10 -- --backend=$BACKEND --grid=$GRID',
-            # TODO
-        ]
+    executable = 'pytest'
+    executable_opts = [
+        "-v -m continuous_benchmarking "
+        "--benchmark-only "
+        "--benchmark-json=TESTING.json "
+        "--backend=gtfn_gpu --grid=icon_benchmark "
+        "model/atmosphere/diffusion/tests/diffusion/integration_tests/test_benchmark_diffusion.py::test_run_diffusion_benchmark[grid0]" 
+    ]
