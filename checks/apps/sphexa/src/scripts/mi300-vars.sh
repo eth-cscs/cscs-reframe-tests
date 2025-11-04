8867181dcf90d55d31ecfc9a2773e20528a91fc4
Jg_Piccinali <>
2025-11-04 15:40:29 +0100

    Update sphexa for AMD

--- checks/apps/sphexa/src/scripts/mi300-vars.sh
+++ checks/apps/sphexa/src/scripts/mi300-vars.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 export LOCAL_RANK=$SLURM_LOCALID
 export GLOBAL_RANK=$SLURM_PROCID
 export GPUS=(0 1 2 3)
@@ -10,4 +11,3 @@ export HIP_VISIBLE_DEVICES=${GPUS[$NUMA_NODE]}
 export MPICH_GPU_SUPPORT_ENABLED=1
 
 numactl --cpunodebind=$NUMA_NODE --membind=$NUMA_NODE "$@"
-#numactl --membind=$NUMA_NODE "$@"
