7e5dcb181dc9735ba7b41123bd452c9f60ab072c
Sebastian Keller <44800242+sekelle@users.noreply.github.com>
2025-11-06 22:14:46 +0100

    Update sphexa test for MI(2,3)00 (#426)

---
+++ checks/apps/sphexa/src/scripts/mi300-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LOCAL_RANK=$SLURM_LOCALID
+export GLOBAL_RANK=$SLURM_PROCID
+export GPUS=(0 1 2 3)
+export NUMA_NODE=$(echo "$LOCAL_RANK % 4" | bc)
+export HIP_VISIBLE_DEVICES=${GPUS[$NUMA_NODE]}
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+numactl --cpunodebind=$NUMA_NODE --membind=$NUMA_NODE "$@"
