58219fdf23eec95cbe850c1b63c2ba4dd9aefbb9
Mikael Simberg <simberg@cscs.ch>
2025-11-14 11:53:01 +0100

    Add missing slurm mpi option mixin

---
+++ checks/mixins/slurm_mpi_options.py
@@ -0,0 +1,27 @@
+# Copyright 2016-2023 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import reframe as rfm
+
+
+class SlurmMpiOptionsMixin(rfm.RegressionMixin):
+    @run_after('setup')
+    def set_slurm_mpi_options(self):
+        features = self.current_environ.features
+        if "openmpi" in features:
+            self.job.launcher.options += ['--mpi=pmix']
+
+            # Disable MCA components to avoid warnings
+            self.env_vars.update(
+                {
+                    'PMIX_MCA_psec': '^munge',
+                    'PMIX_MCA_gds': '^shmem2'
+                }
+            )
+        elif "cray-mpich" in features:
+            self.job.launcher.options += ['--mpi=cray_shasta']
+        else:
+            # Assume cray-mpich is used if nothing is specified.
+            self.job.launcher.options += ['--mpi=cray_shasta']
