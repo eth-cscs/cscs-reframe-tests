d92b2a200157150258c4fd274db3f03636576fd2
Jean-guillaume Piccinali <jgphpc@users.noreply.github.com>
2025-11-24 11:23:43 +0100

    formatting

--- checks/microbenchmarks/gpu/gpu_benchmarks/fft.py
+++ checks/microbenchmarks/gpu/gpu_benchmarks/fft.py
@@ -12,17 +12,15 @@ from uenv import uarch
 
 @rfm.simple_test
 class FFTBenchBuild(rfm.CompileOnlyRegressionTest):
-    '''
-    Build FFT benchmark
-    '''
-    maintainers = ['SSA']
+    descr = 'Build FFT benchmark'
+    maintainers = ['AdhocMan', 'SSA']
     sourcesdir = None
+    valid_systems = ['+uenv +amdgpu', '+uenv +nvgpu']
     valid_prog_environs = ['+uenv +prgenv +rocm', '+uenv +prgenv +cuda']
-    valid_systems = ['+uenv']
     build_system = 'CMake'
-    # TODO: change to original repo once PR is merged
     prebuild_cmds = [
-        'git clone --depth 1 -b reframe-ci https://github.com/eth-cscs/gpu-benchmarks.git'
+        'git clone --depth 1 -b reframe-ci '
+        'https://github.com/eth-cscs/gpu-benchmarks.git'
     ]
     time_limit = '4m'
     build_locally = False
@@ -53,15 +51,13 @@ class FFTBenchBuild(rfm.CompileOnlyRegressionTest):
         return os.path.isfile(self.executable)
 
 
-
 @rfm.simple_test
 class FFTCheck(rfm.RunOnlyRegressionTest):
-    maintainers = ['SSA']
+    maintainers = ['AdhocMan', 'SSA']
+    valid_systems = ['+uenv +amdgpu', '+uenv +nvgpu']
     valid_prog_environs = ['+uenv +prgenv +rocm', '+uenv +prgenv +cuda']
-    valid_systems = ['+uenv']
     time_limit = '4m'
     tags = {'production', 'uenv', 'bencher'}
-
 
     fft_dim = parameter(['1D', '2D', '3D'])
     fft_size = parameter([128, 256, 512, 1024])
@@ -69,23 +65,23 @@ class FFTCheck(rfm.RunOnlyRegressionTest):
     # runtime values in ms
     reference_timings = {
         '1D': {
-            'mi200': {'128': 0.179489, '256':  0.333746, '512': 0.667365, '1024': 1.33991},
-            'mi300': {'128': 0.0726874, '256': 0.210958, '512': 0.32853, '1024': 0.601683},
-            'gh200': {'128': 0.0606304, '256': 0.117171, '512': 0.228963, '1024': 0.459949},
+            'mi200': {'128': 0.179489, '256':  0.333746, '512': 0.667365, '1024': 1.33991},  # noqa: E501
+            'mi300': {'128': 0.0726874, '256': 0.210958, '512': 0.32853, '1024': 0.601683},  # noqa: E501
+            'gh200': {'128': 0.0606304, '256': 0.117171, '512': 0.228963, '1024': 0.459949},  # noqa: E501
         },
         '2D': {
-            'mi200': {'128': 0.432062, '256': 1.86332, '512': 8.9016, '1024': 54.3387},
-            'mi300': {'128': 0.180459, '256': 0.879223, '512': 3.80603, '1024': 23.7081},
-            'gh200': {'128': 0.155469, '256': 0.58984, '512': 2.32393, '1024': 10.7417},
+            'mi200': {'128': 0.432062, '256': 1.86332, '512': 8.9016, '1024': 54.3387},  # noqa: E501
+            'mi300': {'128': 0.180459, '256': 0.879223, '512': 3.80603, '1024': 23.7081},  # noqa: E501
+            'gh200': {'128': 0.155469, '256': 0.58984, '512': 2.32393, '1024': 10.7417},  # noqa: E501
         },
         '3D': {
-            'mi200': {'128': 0.23045, '256': 1.7439, '512': 16.0939, '1024': 161.762},
-            'mi300': {'128': 0.0794162, '256': 0.673843, '512': 5.85871, '1024': 75.8344},
-            'gh200': {'128': 0.0550848, '256': 0.472509, '512': 3.68538, '1024': 36.0022},
+            'mi200': {'128': 0.23045, '256': 1.7439, '512': 16.0939, '1024': 161.762},  # noqa: E501
+            'mi300': {'128': 0.0794162, '256': 0.673843, '512': 5.85871, '1024': 75.8344},  # noqa: E501
+            'gh200': {'128': 0.0550848, '256': 0.472509, '512': 3.68538, '1024': 36.0022},  # noqa: E501
         }
     }
 
-    # Batch size to use in each dimension. 
+    # Batch size to use in each dimension
     batch_sizes = {
         '1D': 50000,
         '2D': 500,
@@ -99,6 +95,7 @@ class FFTCheck(rfm.RunOnlyRegressionTest):
         read_bytes = self.fft_size * 16.0
         if self.fft_dim == '2D':
             read_bytes *= self.fft_size * 2.0
+
         if self.fft_dim == '3D':
             read_bytes *= self.fft_size * self.fft_size * 3.0
 
@@ -119,7 +116,12 @@ class FFTCheck(rfm.RunOnlyRegressionTest):
             size_arg += f' {self.fft_size}'
         if self.fft_dim == '3D':
             size_arg += f' {self.fft_size} {self.fft_size}'
-        self.executable_opts = [f'-b {self.batch_sizes[self.fft_dim]}', '-p double', '-s 10', f'-n {size_arg}']
+        self.executable_opts = [
+            f'-b {self.batch_sizes[self.fft_dim]}',
+            '-p double',
+            '-s 10',
+            f'-n {size_arg}'
+        ]
 
     @sanity_function
     def assert_reference(self):
@@ -128,7 +130,8 @@ class FFTCheck(rfm.RunOnlyRegressionTest):
     @run_before('performance')
     def set_perf_vars(self):
         make_perf = sn.make_performance_function
-        runtime = sn.extractsingle('Mean time \[ms\]: (?P<time>\S+)', self.stdout, 'time', float)
+        runtime = sn.extractsingle(
+            r'Mean time \[ms\]: (?P<time>\S+)', self.stdout, 'time', float)
         bandwidth = self.runtime_to_bandwidth(runtime)
 
         self.perf_variables = {
@@ -139,7 +142,7 @@ class FFTCheck(rfm.RunOnlyRegressionTest):
     def set_references(self):
         self.uarch = uarch(self.current_partition)
 
-        runtime = self.reference_timings.get(self.fft_dim, {}).get(self.uarch, {}).get(str(self.fft_size))
+        runtime = self.reference_timings.get(self.fft_dim, {}).get(self.uarch, {}).get(str(self.fft_size))  # noqa: E501
 
         if runtime is not None:
             bandwidth = self.runtime_to_bandwidth(runtime)
@@ -149,4 +152,3 @@ class FFTCheck(rfm.RunOnlyRegressionTest):
                     'bandwidth': (bandwidth, -0.2, None, 'TB/s'),
                 }
             }
-
