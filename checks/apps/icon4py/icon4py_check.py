2271156a10f41f73b48b313271e2c12458c26162
Christos Kotsalos <christos.kotsalos@cscs.ch>
2025-11-26 14:54:57 +0100

    ICON4Py check: apps folder & uenv agnostic

--- checks/apps/icon4py/icon4py_check.py
+++ checks/apps/icon4py/icon4py_check.py
@@ -3,8 +3,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-import os
-
 import reframe as rfm
 import reframe.utility.sanity as sn
 
@@ -19,12 +17,15 @@ class ICON4PyBenchmarks(rfm.RunOnlyRegressionTest):
     time_limit = '90m'
     build_locally = False
     env_vars = {
+        'ICON4PY_PYTHON_VERSION': '3.11',
         'UV_NO_CACHE': '1',
         'UV_CACHE_DIR': '$SCRATCH/.cache/uv',
         'CC': '$(which gcc)',
         'MPICH_CC': '$(which gcc)',
         'CXX': '$(which g++)',
         'MPICH_CXX': '$(which g++)',
+        'HUGETLB_ELFMAP': 'no',
+        'HUGETLB_MORECORE': 'no',
         # GT4Py cache does not work properly for dace backend yet
         # 'GT4PY_BUILD_CACHE_LIFETIME': 'persistent',
         # 'GT4PY_BUILD_CACHE_DIR': '...',
@@ -33,35 +34,31 @@ class ICON4PyBenchmarks(rfm.RunOnlyRegressionTest):
     executable_opts = ['2>&1']
 
     @run_before('run')
-    def install_deps(self):
-        self.prerun_cmds = ['./_install.sh &> _install.sh.log 2>&1']
-
-    @run_before('run')
     def prepare_env(self):
         gpu_arch = self.current_partition.select_devices('gpu')[0].arch
-        if 'gfx' in gpu_arch:
+        if 'gfx' in gpu_arch:  # AMD GPU
             self.env_vars['CUPY_INSTALL_USE_HIP'] = '1'
             self.env_vars['HCC_AMDGPU_TARGET'] = gpu_arch
             self.env_vars['ROCM_HOME'] = '/user-environment/env/default'
+        # elif 'sm_' in gpu_arch:  # CUDA GPU
+
+    @run_before('run')
+    def install_deps(self):
+        self.prerun_cmds = ['./_install.sh &> _install.sh.log 2>&1']
 
     @sanity_function
     def validate_test(self):
-        rfm_stop = os.getenv('RFM_ICON4PY_STOP')
-        if rfm_stop == 'Y':
-            diffusion_granule = sn.assert_found('# INFO:', self.stdout)
-            dycore_granule = True
-        else:
-            diffusion_granule = sn.assert_found(
-                (r'^\s*model/atmosphere/diffusion/tests/'
-                 r'diffusion/integration_tests/'
-                 r'test_benchmark_diffusion\.py'
-                 r'::test_diffusion_benchmark\s*PASSED'
-                 ), self.stdout)
-            dycore_granule = sn.assert_found(
-                (r'^\s*model/atmosphere/dycore/tests/'
-                 r'dycore/integration_tests/test_benchmark_solve_nonhydro\.py'
-                 r'::test_benchmark_solve_nonhydro\[True-False\]\s*PASSED'),
-                self.stdout)
+        diffusion_granule = sn.assert_found(
+            (r'^\s*model/atmosphere/diffusion/tests/'
+             r'diffusion/integration_tests/'
+             r'test_benchmark_diffusion\.py'
+             r'::test_diffusion_benchmark\s*PASSED'
+             ), self.stdout)
+        dycore_granule = sn.assert_found(
+            (r'^\s*model/atmosphere/dycore/tests/'
+             r'dycore/integration_tests/test_benchmark_solve_nonhydro\.py'
+             r'::test_benchmark_solve_nonhydro\[True-False\]\s*PASSED'
+             ), self.stdout)
 
         return diffusion_granule and dycore_granule
 
--- checks/apps/icon4py/src/_run.sh
+++ checks/apps/icon4py/src/_run.sh
@@ -1,42 +1,24 @@
 #!/bin/bash
 
-py_version=$(python3 --version 2>&1 |awk '{print $2}')
-minor_version=$(echo "$py_version" |cut -d \. -f2)
-# echo "$py_version $minor_version"
+date
 
-if [ "$minor_version" -gt "13" ] ;then export RFM_ICON4PY_STOP='Y' ; else export RFM_ICON4PY_STOP='N' ; fi
+unset PYTHONPATH
 
-if [ "$RFM_ICON4PY_STOP" == "Y" ] ;then
-    echo "# INFO: $0: python/$py_version is incompatible with this test (try python/3.13), exiting..."
-    # for sanity:
-    echo "model/atmosphere/diffusion/tests/diffusion/integration_tests/test_benchmark_diffusion.py::test_diffusion_benchmark PASSED [100%] # FAKE"
-    echo "model/atmosphere/dycore/tests/dycore/integration_tests/test_benchmark_solve_nonhydro.py::test_benchmark_solve_nonhydro[True-False] PASSED [100%] # FAKE"
-    # for performance:                                  Min     Max     Mean    StdDev
-    echo "test_diffusion_benchmark                      0.0000  0.0000  0.0000  0.0000  # FAKE"
-    echo "test_benchmark_solve_nonhydro[True-False]     0.0000  0.0000  0.0000  0.0000  # FAKE"
-    sleep 4
-    exit 0
+cd icon4py
+source .venv/bin/activate
 
-else
+pytest -v \
+    -m continuous_benchmarking \
+    --benchmark-only \
+    --benchmark-warmup=on \
+    --benchmark-warmup-iterations=30 \
+    --benchmark-json=icon4py_benchmarks.json \
+    --backend=dace_gpu \
+    --grid=icon_benchmark_regional \
+    --benchmark-time-unit=ms \
+    model/atmosphere/diffusion/tests/diffusion/integration_tests/test_benchmark_diffusion.py::test_diffusion_benchmark \
+    model/atmosphere/dycore/tests/dycore/integration_tests/test_benchmark_solve_nonhydro.py::test_benchmark_solve_nonhydro[True-False]
+echo
 
-    date
-    cd icon4py
-    source .venv/bin/activate
-
-    pytest -v \
-        -m continuous_benchmarking \
-        --benchmark-only \
-        --benchmark-warmup=on \
-        --benchmark-warmup-iterations=30 \
-        --benchmark-json=icon4py_benchmarks.json \
-        --backend=dace_gpu \
-        --grid=icon_benchmark_regional \
-        --benchmark-time-unit=ms \
-        model/atmosphere/diffusion/tests/diffusion/integration_tests/test_benchmark_diffusion.py::test_diffusion_benchmark \
-        model/atmosphere/dycore/tests/dycore/integration_tests/test_benchmark_solve_nonhydro.py::test_benchmark_solve_nonhydro[True-False]
-    echo
-
-    echo "# run done"
-    date
-
-fi
+echo "# run done"
+date
