1143bc45c840545d5427394b4121e7f97be2d96b
Jean-guillaume Piccinali <jgphpc@users.noreply.github.com>
2026-02-09 13:56:53 +0100

    Skip uenv

--- checks/system/slurm/invalid_acc.py
+++ checks/system/slurm/invalid_acc.py
@@ -1,7 +1,9 @@
-# Copyright 2026 ETHZ/CSCS
+# Copyright ETHZ/CSCS
 # See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
+
+import os
 
 import reframe as rfm
 import reframe.utility.sanity as sn
@@ -10,12 +12,19 @@ import reframe.utility.sanity as sn
 @rfm.simple_test
 class InvalidAccount(rfm.RunOnlyRegressionTest):
     valid_systems = ['-remote']
-    valid_prog_environs = ['builtin']
+    valid_prog_environs = ['+builtin']
     local = True
-    executable = 'srun -N1 -A abcd1234 echo FAILED'
+    sourcesdir = None
+    executable = 'srun'
+    executable_opts = [
+        '--account=abcd1234', 'echo', 'this job should never start']
     tags = {'production', 'maintenance'}
     maintainers = ['VCUE', 'perettig']
 
+    @run_before('setup')
+    def skip_on_uenv(self):
+        self.skip_if('UENV' in os.environ, 'Skipping when UENV is set')
+
     @sanity_function
     def validate(self):
-        return sn.assert_found(r'nvalid account', self.stderr) 
+        return sn.assert_found(r'nvalid account', self.stderr)
