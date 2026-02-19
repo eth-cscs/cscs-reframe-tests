2b175a379177d388011d6601d05008b057e439bb
Guilherme Peretti-Pezzi <peretti@cscs.ch>
2026-02-19 14:24:56 +0100

    fix remark and syntax

--- config/systems/lys.py
+++ config/systems/lys.py
@@ -10,87 +10,84 @@
 
 import os
 
-base_config = {
-    'modules_system': 'lmod',
-    'resourcesdir': '/vast/scratch/firecr02/reframe/resources',
-    'partitions': [        
-        {
-            'name': 'normal',
-            'descr': 'GH200',
-            'scheduler': 'slurm',
-            'time_limit': '10m',
-            'environs': [
-                'builtin',
-#                'PrgEnv-cray',
-#                'PrgEnv-gnu',
-#                'PrgEnv-ce',
-                # FIXME: Problem loading the following environments
-                # 'PrgEnv-nvidia',
-                # 'PrgEnv-nvhpc'
-            ],
-            'max_jobs': 100,
-            'extras': {
-                'cn_memory': 825,
-            },
-            'features': [
-                'ce', 'gpu', 'nvgpu', 'remote', 'scontrol', 'uenv',
-                'hugepages_slurm'],
-            'resources': [
-                {
-                    'name': 'switches',
-                    'options': ['--switches={num_switches}']
-                },
-                {
-                    'name': 'gres',
-                    'options': ['--gres={gres}']
-                },
-                {
-                    'name': 'memory',
-                    'options': ['--mem={mem_per_node}']
-                },
-                {
-                    'name': 'cpe_ce_image',
-                    'options': [
-                        '--container-image={image}',
-                     ]
-                },
-                {
-                    'name': 'cpe_ce_mount',
-                    'options': [
-                        # Mount both the stagedir and the directory related
-                        # used 3 levels above (the one related to the system)
-                        # to be able to find fixtures
-                        '--container-mounts={stagedir}/../../../,'  # split
-                        '{stagedir}:/rfm_workdir',
-                        '--container-workdir=/rfm_workdir'
-                     ]
-                },
-                {
-                    'name': 'cpe_ce_extra_mounts',
-                    'options': [
-                        '--container-mounts={mount}:{mount}',
-                     ]
-                }
-            ],
-            'devices': [
-                {
-                    'type': 'gpu',
-                    'arch': 'sm_90',
-                    'num_devices': 4
-                }
-            ],
-            'launcher': 'srun',
-        },
-    ]
-}
-
-base_config['name'] = 'lys'
-base_config['descr'] = 'Lys vCluster'
-base_config['hostnames'] = ['lys']
-
 site_configuration = {
     'systems': [
-        base_config,
+         {
+            'name': 'lys',
+            'descr': 'Lys vcluster',
+            'hostnames': ['lys'],
+            'modules_system': 'lmod',
+            'resourcesdir': '/vast/scratch/firecr02/reframe/resources',
+            'partitions': [        
+                {
+                    'name': 'normal',
+                    'descr': 'GH200',
+                    'scheduler': 'slurm',
+                    'time_limit': '10m',
+                    'environs': [
+                        'builtin',
+                        # 'PrgEnv-cray',
+                        # 'PrgEnv-gnu',
+                        # 'PrgEnv-ce',
+                        # FIXME: Problem loading the following environments
+                        # 'PrgEnv-nvidia',
+                        # 'PrgEnv-nvhpc'
+                    ],
+                    'max_jobs': 100,
+                    'extras': {
+                        'cn_memory': 825,
+                    },
+                    'features': [
+                        'ce', 'gpu', 'nvgpu', 'remote', 'scontrol', 'uenv',
+                        'hugepages_slurm'],
+                    'resources': [
+                        {
+                            'name': 'switches',
+                            'options': ['--switches={num_switches}']
+                        },
+                        {
+                            'name': 'gres',
+                            'options': ['--gres={gres}']
+                        },
+                        {
+                            'name': 'memory',
+                            'options': ['--mem={mem_per_node}']
+                        },
+                        {
+                            'name': 'cpe_ce_image',
+                            'options': [
+                                '--container-image={image}',
+                            ]
+                        },
+                        {
+                            'name': 'cpe_ce_mount',
+                            'options': [
+                                # Mount both the stagedir and the directory related
+                                # used 3 levels above (the one related to the system)
+                                # to be able to find fixtures
+                                '--container-mounts={stagedir}/../../../,'  # split
+                                '{stagedir}:/rfm_workdir',
+                                '--container-workdir=/rfm_workdir'
+                            ]
+                        },
+                        {
+                            'name': 'cpe_ce_extra_mounts',
+                            'options': [
+                                '--container-mounts={mount}:{mount}',
+                            ]
+                        }
+                    ],
+                    'devices': [
+                        {
+                            'type': 'gpu',
+                            'arch': 'sm_90',
+                            'num_devices': 4
+                        }
+                    ],
+                    'launcher': 'srun',
+                },
+            ]
+        },
     ],
     'environments': [        
     ],
