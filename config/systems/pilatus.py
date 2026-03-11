0febb2e34027a6227ceb80d61c687c3bcb8f6e0a
Guilherme <gppezzi@users.noreply.github.com>
2026-03-11 17:10:28 +0100

    Add max_local_jobs to vcluster config files (#542)

--- config/systems/pilatus.py
+++ config/systems/pilatus.py
@@ -13,6 +13,7 @@ import copy
 base_config = {
     'modules_system': 'lmod',
     'resourcesdir': '/capstor/store/cscs/cscs/public/reframe/resources',
+    'max_local_jobs': 20,
     'partitions': [
         {
             'name': 'login',
