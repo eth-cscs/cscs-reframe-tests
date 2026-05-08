ab51bc72dfea5a91b4a1062415588e3cc88ffc90
Jg_Piccinali <>
2026-05-08 18:49:12 +0200

    Fix for review

--- checks/apps/paraview/build-gadget-plugin/paraview_buildgadgetplugin.py
+++ checks/apps/paraview/build-gadget-plugin/paraview_buildgadgetplugin.py
@@ -12,14 +12,26 @@ from packaging.version import Version
 from packaging.specifiers import SpecifierSet
 
 
+_UENV_CLI = 'uenv'
+
+
 def uenv_metadata():
     import os
     import json
     from reframe.utility import osext
 
     uenv_label = os.environ['UENV']
-    metadata = json.loads(osext.run_command(f"uenv image inspect --json {uenv_label}").stdout)
-    return Version(metadata["version"]), Version(metadata["tag"])
+    _uenv_version = osext.run_command(f'{_UENV_CLI} --version').stdout.strip()
+
+    if Version(_uenv_version) >= Version('9.2.0'):
+        _cmd = f"{_UENV_CLI} image inspect --json {uenv_label}"
+        metadata = json.loads(osext.run_command(_cmd, shell=True).stdout)
+        return Version(metadata["version"]), Version(metadata["tag"])
+    else:
+        _cmd = f"{_UENV_CLI} image inspect --format=\'{{version}} {{tag}}\' {uenv_label}"  # noqa E501
+        _version_tag = osext.run_command(_cmd, shell=True).stdout
+        return str(_version_tag.split(" ")[0]), \
+            str(_version_tag.split(" ")[1])
 
 
 @rfm.simple_test
@@ -29,6 +41,9 @@ class ParaviewBuildGadgetPlugin(rfm.CompileOnlyRegressionTest):
 
     build_system = 'CMake'
     build_locally = False
+    repo = variable(str,
+                    value='https://github.com/jfavre/ParaViewGadgetPlugin.git')
+    commit = variable(str, value='d9d12bde19454199b733b4e1285be3dcd1ac5595')
 
     num_tasks = 1
     num_tasks_per_node = 1
@@ -45,14 +60,11 @@ class ParaviewBuildGadgetPlugin(rfm.CompileOnlyRegressionTest):
 
     @run_before('compile')
     def prepare_build(self):
-        commit = "d9d12bde19454199b733b4e1285be3dcd1ac5595"
         self.prebuild_cmds = [
-            "mkdir gadget-plugin.git; pushd gadget-plugin.git",
-            "git init",
-            "git remote add origin https://github.com/jfavre/ParaViewGadgetPlugin",
-            "git fetch --depth 1 origin {}".format(commit),
-            "git reset --hard FETCH_HEAD",
-            "popd",
+            f"mkdir gadget-plugin.git", "cd gadget-plugin.git",
+            f"git init", f"git remote add origin {self.repo}",
+            f"git fetch --depth 1 origin {self.commit}",
+            f"git reset --hard FETCH_HEAD", "cd .."
         ]
 
         if (fix_actions := need_fix_hdf5vtk(self)) is not None:
@@ -62,13 +74,15 @@ class ParaviewBuildGadgetPlugin(rfm.CompileOnlyRegressionTest):
         self.build_system.cxx = "g++"
         self.build_system.builddir = "build"
         self.build_system.configuredir = "gadget-plugin.git"
+        self.env_vars["Python3_ROOT_DIR"] = \
+            '$(dirname $(which python3) |xargs dirname)'
 
     @sanity_function
     def validate_build(self):
         return sn.all(
             [
                 sn.assert_eq(
-                    sn.count(sn.glob(r'**/libGadgetReader.so', recursive=True)),
+                    sn.count(sn.glob(r'**/libGadgetReader.so', recursive=True)),  # noqa: E501
                     1,
                 ),
                 sn.assert_eq(
@@ -79,68 +93,21 @@ class ParaviewBuildGadgetPlugin(rfm.CompileOnlyRegressionTest):
         )
 
 
-# External VTK and hdf5
-
-
-def has_bug_hdf5vtk(test):
-    """
-    This tells if upstream paraview has the bug of VTK::hdf5 when used as external.
-    """
-    version, _ = test.uenv_version
-
-    if version in SpecifierSet("~=6.1"):
-        return True
-    elif version in SpecifierSet("~=6.0"):
-        return True
-
-    return False
-
-
 def need_fix_hdf5vtk(test):
     """
-    This tells which fix should be applied according to how bug has been workarounded in uenv.
+    Patch fix code according to ParaView version
     """
     version, _ = test.uenv_version
 
     if version in SpecifierSet("~=6.1"):
-        # this is a newer workaround that avoids warning about deprecated targets "vtk*"
-        return [
-            dedent(
-                """
-                patch -p 1 -d gadget-plugin.git <<-'EOF'
-                    diff --git a/src/Reader/vtk.module b/src/Reader/vtk.module
-                    index 46b504d..ed640bc 100644
-                    --- a/src/Reader/vtk.module
-                    +++ b/src/Reader/vtk.module
-                    @@ -12,4 +12,4 @@ DEPENDS
-                     PRIVATE_DEPENDS
-                       VTK::vtksys
-                       VTK::mpi
-                    -  VTK::hdf5
-                    +  VTK::hdf5vtk
-                EOF
-                """
-            )
+        _patch_cmd = [
+            'patch -p 1 -d gadget-plugin.git -i ../fix_reader_v61.patch'
         ]
     elif version in SpecifierSet("~=6.0"):
-        # this is the first workaround applied. it works, but there is a complain about
-        # deprecated targets due to the name chosen, because it starts with "vtk*"
-        return [
-            dedent(
-                """
-                patch -p 1 -d gadget-plugin.git <<-'EOF'
-                    diff --git a/src/Reader/vtk.module b/src/Reader/vtk.module
-                    index 46b504d..ed640bc 100644
-                    --- a/src/Reader/vtk.module
-                    +++ b/src/Reader/vtk.module
-                    @@ -12,4 +12,4 @@ DEPENDS
-                     PRIVATE_DEPENDS
-                       VTK::vtksys
-                       VTK::mpi
-                    -  VTK::hdf5
-                    +  VTK::vtkhdf5
-                EOF
-                """
-            )
+        _patch_cmd = [
+            'patch -p 1 -d gadget-plugin.git -i ../fix_reader_v60.patch'
         ]
-    return None
+    else:
+        _patch_cmd = None
+
+    return _patch_cmd
