55e3d1a47375decdb3aa7b0f92778c9f1c571e91
Guilherme Peretti-Pezzi <peretti@cscs.ch>
2026-07-14 17:06:53 +0200

    Refresh ReFrame container in preparation for TRE runs

---
+++ containers/reframe/submit-reframe-lys.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH --account=csstaff
+#SBATCH --job-name=test-reframe
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+
+srun --environment=./reframe.toml reframe -C /opt/cscs-reframe-tests/config/cscs.py -c /opt/cscs-reframe-tests/checks/ --system lys:normal -n SlurmGPUGresTest -r
