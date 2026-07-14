484c9e6eb733b2fc20390f5bc8daa4b4cbba799e
Guilherme Peretti-Pezzi <peretti@cscs.ch>
2026-07-14 17:39:21 +0200

    minor fix

--- containers/reframe/submit-reframe-clariden.sh
+++ containers/reframe/submit-reframe-clariden.sh
@@ -3,6 +3,6 @@
 #SBATCH --account=csstaff
 #SBATCH --job-name=test-reframe
 #SBATCH --time=00:30:00
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 
 srun --environment=./reframe.toml reframe -C /opt/cscs-reframe-tests/config/cscs.py -c /opt/cscs-reframe-tests/checks/ --system clariden:normal -n SlurmGPUGresTest -n NCCLTestsCE -n CudaNodeBurnGemmCE -r
