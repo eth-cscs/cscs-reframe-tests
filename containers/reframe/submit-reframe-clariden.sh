20f2d2c9f5c5547e3b9a76d7519894101e56780f
Guilherme Peretti-Pezzi <peretti@cscs.ch>
2026-07-14 17:37:54 +0200

    add initial TRE tests to the list

--- containers/reframe/submit-reframe-clariden.sh
+++ containers/reframe/submit-reframe-clariden.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=csstaff
 #SBATCH --job-name=test-reframe
-#SBATCH --time=00:10:00
-#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --nodes=2
 
-srun --environment=./reframe.toml reframe -C /opt/cscs-reframe-tests/config/cscs.py -c /opt/cscs-reframe-tests/checks/ --system clariden:normal -n SlurmGPUGresTest -r
+srun --environment=./reframe.toml reframe -C /opt/cscs-reframe-tests/config/cscs.py -c /opt/cscs-reframe-tests/checks/ --system clariden:normal -n SlurmGPUGresTest -n NCCLTestsCE -n CudaNodeBurnGemmCE -r
