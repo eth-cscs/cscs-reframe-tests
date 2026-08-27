4556d33bc44a0b581151e58be764971c1965c3f8
jgphpc <jgp@cscs.ch>
2026-08-27 12:35:44 +0200

    deprecate

---
+++ deprecated_checks/prgenv/uenv_fixincludes.py
@@ -0,0 +1,39 @@
+# Copyright Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.xfail(
+    "ICON uenvs are based on old versions of spack that don't fix GCC include",
+    lambda self: (
+        self.current_environ.name.startswith("icon_25.2_v4")
+        or self.current_environ.name.startswith("icon-dsl_25.12_v2")
+    ),
+)  # noqa:E501
+@rfm.simple_test
+class UenvFixincludes(rfm.RunOnlyRegressionTest):
+    descr = '''
+    Check that GCC is not "fixing" the pthread.h header. See
+    https://github.com/spack/spack-packages/pull/2596 for an overview and links
+    to further details.
+    '''
+
+    valid_systems = ['+uenv +remote +scontrol']
+    valid_prog_environs = ['+uenv']
+    maintainers = ['msimberg', 'SSA']
+
+    time_limit = '1m'
+    num_tasks_per_node = 1
+    sourcesdir = None
+    executable = 'find'
+    executable_opts = ["/user-environment", "/user-tools", "-type", "f",
+                       "-name", "pthread.h"]
+    tags = {'maintenance', 'uenv'}
+
+    @sanity_function
+    def validate(self):
+        return sn.assert_not_found(r'include-fixed', self.stdout)
